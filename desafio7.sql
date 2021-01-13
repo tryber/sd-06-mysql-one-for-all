d5f3d7c6124fc6619fc1f6e1cdbad0046108d02b
Dilenio Enderle <dilenio@gmail.com>
2021-01-13 18:38:16 -0300

    feat: challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,17 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+  a.artist AS `artista`,
+  al.album AS `album`,
+  COUNT(f.user_id) AS `seguidores`
+FROM
+  SpotifyClone.followers AS f
+INNER JOIN
+  SpotifyClone.artists AS a ON a.id = f.artist_id
+INNER JOIN
+  SpotifyClone.albums AS al ON al.artist_id = f.artist_id
+GROUP BY
+  `artista`, `album`
+ORDER BY
+  `seguidores` DESC,
+  `artista` ASC,
+  `album` ASC;
