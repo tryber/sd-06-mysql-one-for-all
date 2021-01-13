97534b0ff4a503391ef87cddf656b1d4c0340575
Luiz Mello <luizmacmillan@gmail.com>
2021-01-13 17:40:43 -0300

    Desafio 10.

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+USE spotifyclone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id int)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE quantidade_musicas INT;
+    SELECT COUNT(*)
+    FROM spotifyclone.historico_de_reproducoes
+    WHERE spotifyclone.historico_de_reproducoes.usuario_id = id INTO quantidade_musicas;
+    RETURN quantidade_musicas;
+END $$
+
+DELIMITER ;
+
+select quantidade_musicas_no_historico(3);
+
