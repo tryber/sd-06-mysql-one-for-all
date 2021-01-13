5d599ec76efe7c477220294e8eabca3b871b8453
Luiz Mello <luizmacmillan@gmail.com>
2021-01-13 18:32:03 -0300

    Correção do Desafio 1.

--- desafio7.sql
+++ desafio7.sql
@@ -2,9 +2,9 @@ CREATE VIEW perfil_artistas AS
 SELECT
 ar.artista,
 ab.album,
-COUNT(s.artista_id) AS `seguidores`
+COUNT(s.artista_id) AS seguidores
 FROM artistas ar
 INNER JOIN albums ab ON ar.artista_id = ab.artista_id
 INNER JOIN seguindo_artistas s ON ar.artista_id = s.artista_id
 GROUP BY ab.album
-ORDER BY `seguidores` DESC, ar.artista ASC, ab.album;
+ORDER BY seguidores DESC, ar.artista ASC, ab.album;
