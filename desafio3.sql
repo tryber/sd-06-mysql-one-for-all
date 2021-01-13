521c41186a709357d592f8d9f03940453fbb0f91
William Gomes <williamjog@hotmail.com>
2021-01-13 18:03:02 -0300

    3º Requisito typo name - nome

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-user.name AS usuario,
-song.name AS nome
+user.nome AS usuario,
+song.nome AS nome
 FROM SpotifyClone.historico_de_reproducoes historico
 INNER JOIN SpotifyClone.usuario user ON historico.usuario_id = user.usuario_id
 INNER JOIN SpotifyClone.cancoes song ON historico.cancoes_id = song.cancoes_id
