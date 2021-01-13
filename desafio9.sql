5af6a4213a48191af3081ce28ba9db7c8b7d0aba
Dilenio Enderle <dilenio@gmail.com>
2021-01-13 19:06:29 -0300

    feat: challenge 9

---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(100))
+BEGIN
+  SELECT
+    ar.artist AS `artista`,
+    al.album AS `album`
+  FROM
+    artists AS ar
+  INNER JOIN
+    albums AS al ON al.artist_id = ar.id
+	WHERE ar.artist like artist_name
+  ORDER BY
+    `album`;
+END; $$
+
+DELIMITER ;
