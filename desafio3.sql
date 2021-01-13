5cd38e910d562e3950bd903eef6f0a052ffe217f
Ana Higo <anahigo.arq@gmail.com>
2021-01-13 17:40:35 -0300

    Correcao Codeclimate

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT 
+SELECT
 U.nome_usuario AS usuario,
 C.nome_cancao AS nome
-FROM usuarios_cancoes AS UC 
+FROM usuarios_cancoes AS UC
 INNER JOIN usuarios AS U
-ON UC.usuario_id = U.usuario_id 
+ON U.usuario_id = UC.usuario_id
 INNER JOIN cancoes AS C
-ON UC.cancao_id= C.cancao_id
+ON C.cancao_id = UC.cancao_id
 ORDER BY usuario, nome;
-  
