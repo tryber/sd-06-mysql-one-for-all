6521dd798fe7d9861014a83a5402ed63842cfdc8
Isa Koga <isadorakoga710@gmail.com>
2021-01-13 17:47:02 -0300

    :memo: Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(DISTINCT SpotifyClone.musicas.musica_id) AS cancoes,
+COUNT(DISTINCT SpotifyClone.artistas.artista_id) AS artistas,
+COUNT(DISTINCT SpotifyClone.albums.album_id) AS albuns
+FROM SpotifyClone.musicas, spotifyClone.artistas, SpotifyClone.albums;
