6736b12fcaddbe7f47387bfe1612f948f9c25bb9
Kesley Lahoz <namielahoz@gmail.com>
2021-01-13 18:40:30 -0300

    desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,12 @@
-CREATE VIEW estatisticas_musicais AS
-SELECT 
-COUNT(DISTINCT c.titulo_cancao) AS 'cancoes',
-COUNT(DISTINCT a.nome_artista) AS 'artistas',
-COUNT(DISTINCT al.titulo_album) AS 'albuns'
-FROM SpotifyClone.cancoes AS c, SpotifyClone.artistas AS a, SpotifyClone.albuns AS al;
+DROP VIEW IF EXISTS `estatisticas_musicais`;
+
+CREATE VIEW `estatisticas_musicais` AS
+SELECT
+  COUNT(DISTINCT(can.nome)) AS `cancoes`,
+  COUNT(DISTINCT(art.nome)) AS `artistas`,
+  COUNT(DISTINCT(alb.nome)) AS `albuns`
+FROM 
+  SpotifyClone.artista AS art,
+  SpotifyClone.album AS alb,
+  SpotifyClone.cancoes AS can;
+  
