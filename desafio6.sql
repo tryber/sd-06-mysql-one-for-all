2f9f8236e3e598147925dcd3e85e3248053f9472
LeandroParisi <lepcbelisario@gmail.com>
2021-01-13 18:47:22 -0300

    Desafios 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT
+CREATE VIEW faturamento_atual AS SELECT
 ROUND(MIN(p.valor_plano), 2) AS `faturamento_minimo`,
 ROUND(MAX(p.valor_plano), 2) AS `faturamento_maximo`,
 ROUND(AVG(p.valor_plano), 2) AS `faturamento_medio`,
