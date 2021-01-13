1518a448cc6e4181ffd8daa73ddd07f834fa5422
William Gomes <williamjog@hotmail.com>
2021-01-13 20:26:50 -0300

    8º requisito

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 CREATE VIEW perfil_artistas AS
 SELECT
-art.nome as artista,
-alb.nome as album,
+art.nome AS artista,
+alb.nome AS album,
 COUNT(*) AS seguidores
 FROM SpotifyClone.seguindo_artistas AS seg, SpotifyClone.artista as art, SpotifyClone.album AS alb
 WHERE seg.artista_id = art.artista_id AND art.artista_id = alb.artista_id
