7ae199a5b5fbf18040021a0417af8fd0d4b57226
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-13 18:38:38 -0300

    desafio7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+SELECT SpotifyClone.artist.artist_name AS artista,
+SpotifyClone.album.album_name AS album,
+COUNT(SpotifyClone.artist.artist_id) AS seguidores
+FROM follow
+INNER JOIN artist
+ON SpotifyClone.artist.artist_id = SpotifyClone.follow.artist_id
+INNER JOIN album
+ON SpotifyClone.artist.artist_id = SpotifyClone.album.artist_id
+GROUP BY SpotifyClone.artist.artist_name, SpotifyClone.album.album_name
+ORDER BY seguidores DESC, artista ASC, album;
