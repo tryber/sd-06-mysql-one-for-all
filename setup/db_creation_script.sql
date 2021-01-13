ad4a7591f50c69ebd22f11019279072adaa6d5c4
LeandroParisi <lepcbelisario@gmail.com>
2021-01-13 17:08:46 -0300

    Desafio 1

--- setup/db_creation_script.sql
+++ setup/db_creation_script.sql
@@ -18,7 +18,7 @@ CREATE SCHEMA IF NOT EXISTS `SpotifyClone` DEFAULT CHARACTER SET utf8 ;
 USE `SpotifyClone` ;
 
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`plano`
+-- Table `mydb`.`plano`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
   `plano_id` INT NOT NULL AUTO_INCREMENT,
@@ -28,8 +28,9 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
   UNIQUE INDEX `plano_id_UNIQUE` (`plano_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`usuarios`
+-- Table `mydb`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuarios` (
   `usuario_id` INT NOT NULL AUTO_INCREMENT,
@@ -41,13 +42,14 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuarios` (
   INDEX `fk_usuarios_plano1_idx` (`plano_id` ASC) VISIBLE,
   CONSTRAINT `fk_usuarios_plano1`
     FOREIGN KEY (`plano_id`)
-    REFERENCES `SpotifyClone`.`plano` (`plano_id`)
+    REFERENCES `mydb`.`plano` (`plano_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`artistas`
+-- Table `mydb`.`artistas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artistas` (
   `artista_id` INT NOT NULL AUTO_INCREMENT,
@@ -56,8 +58,9 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artistas` (
   UNIQUE INDEX `artista_id_UNIQUE` (`artista_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`albuns`
+-- Table `mydb`.`albuns`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`albuns` (
   `album_id` INT NOT NULL AUTO_INCREMENT,
@@ -68,29 +71,31 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`albuns` (
   INDEX `fk_albuns_artistas1_idx` (`artista_id` ASC) VISIBLE,
   CONSTRAINT `fk_albuns_artistas1`
     FOREIGN KEY (`artista_id`)
-    REFERENCES `SpotifyClone`.`artistas` (`artista_id`)
+    REFERENCES `mydb`.`artistas` (`artista_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`cancoes`
+-- Table `mydb`.`cancoes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`cancoes` (
-  `cancao_id` INT NOT NULL AUTO_INCREMENT,
+  `cancao_id` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `album_id` INT NOT NULL,
   INDEX `fk_cancoes_albuns1_idx` (`album_id` ASC) VISIBLE,
   PRIMARY KEY (`cancao_id`),
   CONSTRAINT `fk_cancoes_albuns1`
     FOREIGN KEY (`album_id`)
-    REFERENCES `SpotifyClone`.`albuns` (`album_id`)
+    REFERENCES `mydb`.`albuns` (`album_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`artistas_seguidos`
+-- Table `mydb`.`artistas_seguidos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artistas_seguidos` (
   `usuario_id` INT NOT NULL,
@@ -100,18 +105,19 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artistas_seguidos` (
     CONSTRAINT `PK_artistas_seguidos` PRIMARY KEY (`usuario_id`,`artista_id`),
   CONSTRAINT `fk_usuarios_artistas_usuarios1`
     FOREIGN KEY (`usuario_id`)
-    REFERENCES `SpotifyClone`.`usuarios` (`usuario_id`)
+    REFERENCES `mydb`.`usuarios` (`usuario_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuarios_artistas_artistas1`
     FOREIGN KEY (`artista_id`)
-    REFERENCES `SpotifyClone`.`artistas` (`artista_id`)
+    REFERENCES `mydb`.`artistas` (`artista_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `SpotifyClone`.`historico_de_reproducoes`
+-- Table `mydb`.`historico_de_reproducoes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`historico_de_reproducoes` (
   `usuario_id` INT NOT NULL,
@@ -121,70 +127,17 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`historico_de_reproducoes` (
   CONSTRAINT `PK_historico_reproducoes` PRIMARY KEY (`usuario_id`,`cancao_id`),
   CONSTRAINT `fk_usuarios_cancoes_usuarios1`
     FOREIGN KEY (`usuario_id`)
-    REFERENCES `SpotifyClone`.`usuarios` (`usuario_id`)
+    REFERENCES `mydb`.`usuarios` (`usuario_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuarios_cancoes_cancoes1`
     FOREIGN KEY (`cancao_id`)
-    REFERENCES `SpotifyClone`.`cancoes` (`cancao_id`)
+    REFERENCES `mydb`.`cancoes` (`cancao_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-INSERT INTO SpotifyClone.plano (nome, valor_plano)
-VALUES
-('gratuito', 0),
-('familiar', 7.99),
-('universitário', 5.99);
-
-INSERT INTO SpotifyClone.usuarios(nome, idade, plano_id)
-VALUES
-('Thati', 23, 1),
-('Cintia', 35, 2),
-('Bill', 20, 3),
-('Roger', 45, 1);
-
-INSERT INTO SpotifyClone.artistas(nome)
-VALUES
-('Walter Phoenix'),
-('Peter Strong'),
-('Lance Day'),
-('Freedie Shannon');
-
-INSERT INTO SpotifyClone.albuns(nome, artista_id)
-VALUES
-('Envious', 1),
-('Exuberant', 1),
-('Hallowed Steam', 2),
-('Incadescent', 3),
-('Temporary Culture', 4);
-
-INSERT INTO SpotifyClone.cancoes(nome, album_id)
-VALUES
-('Soul For Us', 1), ('Reflections Of Magic', 1), ('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2), ('Time Fireworks', 2),
-("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3),
-("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4),
-("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
-
-INSERT INTO SpotifyClone.artistas_seguidos(usuario_id, artista_id)
-VALUES
-(1, 1),
-(1, 4),
-(1, 3),
-(2, 1),
-(2, 3),
-(3, 2),
-(3, 1),
-(4, 4);
-
-INSERT INTO SpotifyClone.historico_de_reproducoes(usuario_id, cancao_id)
-VALUES
-(1, 1), (1, 6), (1, 14), (1, 16),
-(2, 13), (2, 17), (2, 2), (2, 15),
-(3, 4), (3, 16), (3, 6),
-(4, 3), (4, 18), (4, 11);
