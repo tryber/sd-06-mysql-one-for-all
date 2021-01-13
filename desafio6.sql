33cf7b7c49bc5577c73841ff81df47ad92d5bdb7
LeandroParisi <lepcbelisario@gmail.com>
2021-01-13 18:46:43 -0300

    Desafios 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT
+ROUND(MIN(p.valor_plano), 2) AS `faturamento_minimo`,
+ROUND(MAX(p.valor_plano), 2) AS `faturamento_maximo`,
+ROUND(AVG(p.valor_plano), 2) AS `faturamento_medio`,
+ROUND(sum(p.valor_plano), 2)
+FROM SpotifyClone.usuarios AS u
+JOIN SpotifyClone.plano AS p ON u.plano_id = p.plano_id;
