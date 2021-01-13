0c8f5cfe5d875e9b27f3e5691f984fe7492841f6
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-01-13 19:58:45 -0300

    Removendo o USE

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,7 @@
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(DISTINCT mu.musica_id) AS cancoes,
 COUNT(DISTINCT ar.artista_id) AS artistas,
 COUNT(DISTINCT al.album_id) AS albuns
-FROM musicas mu,
-albuns al,
-artistas ar;
+FROM SpotifyClone.musicas mu,
+SpotifyClone.albuns al,
+SpotifyClone.artistas ar;
