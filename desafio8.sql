41443b9ad1c4a4bea7ab9a265f2e7083a5b2f3fd
LeandroParisi <lepcbelisario@gmail.com>
2021-01-13 19:57:27 -0300

    Correaçao DUMP 2

--- desafio8.sql
+++ desafio8.sql
@@ -2,11 +2,11 @@ USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-    BEFORE DELETE ON usuarios
-    FOR EACH ROW
+BEFORE DELETE ON usuarios
+FOR EACH ROW
 BEGIN
-	DELETE FROM historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM artistas_seguidos WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM artistas_seguidos WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
