28a61235ca8ad622a09477c9293c5bd5ecc4c4f9
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-13 17:53:07 -0300

    desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(DISTINCT SpotifyClone.song.song_name) AS cancoes,
-COUNT(DISTINCT SpotifyClone.artict.artist_name) AS artistas,
+COUNT(DISTINCT SpotifyClone.artist.artist_name) AS artistas,
 COUNT(DISTINCT SpotifyClone.album.album_name) AS albuns
 FROM SpotifyClone.song, SpotifyClone.artist, SpotifyClone.album;
