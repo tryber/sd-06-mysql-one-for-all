e983104c8f35a9e34485f2b3862ad1bad2e21d83
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-13 20:54:26 -0300

    Desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,6 @@
-DROP VIEW IF EXISTS `estatisticas_musicais`;
-
-CREATE VIEW `estatisticas_musicais` AS
+CREATE VIEW estatisticas_musicais AS
 SELECT
-  COUNT(DISTINCT(can.nome)) AS `cancoes`,
-  COUNT(DISTINCT(art.nome)) AS `artistas`,
-  COUNT(DISTINCT(alb.nome)) AS `albuns`
-FROM 
-  SpotifyClone.artista AS art,
-  SpotifyClone.album AS alb,
-  SpotifyClone.cancoes AS can;
+COUNT(DISTINCT(can.nome)) AS 'cancoes',
+COUNT(DISTINCT(art.nome)) AS 'artistas',
+COUNT(DISTINCT(alb.nome)) AS 'albuns'
+FROM SpotifyClone.cancoes AS can, SpotifyClone.artista AS art, SpotifyClone.album AS alb;
