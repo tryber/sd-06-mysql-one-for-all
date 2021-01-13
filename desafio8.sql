964e70071a5b9e8d423d2bb9d0b16ed0aa3c94f2
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-13 20:00:37 -0300

    ajusting desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,6 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM usuario WHERE usuario_id = OLD.usuario_id
-END
-DELIMITER $$;
+DELETE FROM usuario WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
