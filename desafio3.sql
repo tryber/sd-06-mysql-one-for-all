292cdf339c890abdcdf407d8ac480f18dd0bd042
Ana Higo <anahigo.arq@gmail.com>
2021-01-13 17:13:19 -0300

    Correcao Codeclimate

--- desafio3.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT 
-U.nome_usuario AS usuario
-FROM usuarios AS U
-INNER JOIN 
-  FROM sakila.address AS A
-  INNER JOIN sakila.city AS C
-  ON C.city_id = A.city_id
-  ORDER BY C.city;
