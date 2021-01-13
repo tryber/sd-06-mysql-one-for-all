17adb2748db8136a63d7f6d7b500c6b0a27ea440
Júlia Pôrto <juportob@gmail.com>
2021-01-13 19:00:05 -0300

    Desafio 3 - fix code

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,4 @@ FROM SpotifyClone.users AS U,
 SpotifyClone.songs AS S,
 SpotifyClone.reproduction_history AS R
 WHERE U.user_id = R.user_id AND S.song_id = R.song_id
-ORDER BY usuario;
+ORDER BY usuario, nome;
