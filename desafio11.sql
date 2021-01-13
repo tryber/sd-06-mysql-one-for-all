3e4ddccd48ab6cde573b838568ccb0087f2ac7c8
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-13 19:38:48 -0300

    ajusting desafio 7, 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,10 @@
 CREATE VIEW cancoes_premium AS
 SELECT 
-r.historico AS nome,
-COUNT(r.historico) AS reproducoes
+c.cancao AS nome,
+COUNT(r.cancao_id) AS reproducoes
 FROM reproducoes r
 INNER JOIN usuario u ON r.usuario_id = u.usuario_id
+INNER JOIN cancoes c ON r.cancao_id = c.cancao_id
 WHERE u.plano_id <> 1
-GROUP BY nome, historico
+GROUP BY nome, c.cancao
 ORDER BY nome;
--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
 CREATE VIEW perfil_artistas AS
 SELECT 
-artista AS artista,
-album AS album,
+ar.artista AS artista,
+al.album AS album,
 COUNT(usuario_id) AS seguidores
 FROM artistas ar
-INNER JOIN seguidores s ON ar.artista = s.seguindo
+INNER JOIN seguidores s ON ar.artista_id = s.artista_id
 INNER JOIN albuns al ON ar.artista_id = al.artista_id
 GROUP BY artista, album
 ORDER BY seguidores DESC, artista, album;
