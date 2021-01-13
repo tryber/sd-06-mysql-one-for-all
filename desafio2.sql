6332abc6337113811b025111521184a70c658f0e
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-13 17:40:33 -0300

    desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,5 @@
 USE SpotifyClone;
-SELECT COUNT(DISTINCT song_name) as cancoes,
-COUNT(DISTINCT artist_name) as artistas,
-COUNT(DISTINCT album_name) as albuns from song, artist, album;
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(DISTINCT song_name) AS cancoes,
+COUNT(DISTINCT artist_name) AS artistas,
+COUNT(DISTINCT album_name) AS albuns FROM song, artist, album;
