20dfdba0edbbada9ab61d606495a12c62beff244
Ana Higo <anahigo.arq@gmail.com>
2021-01-13 17:38:15 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT 
+U.nome_usuario AS usuario,
+C.nome_cancao AS nome
+FROM usuarios_cancoes AS UC 
+INNER JOIN usuarios AS U
+ON UC.usuario_id = U.usuario_id 
+INNER JOIN cancoes AS C
+ON UC.cancao_id= C.cancao_id
+ORDER BY usuario, nome;
+  
