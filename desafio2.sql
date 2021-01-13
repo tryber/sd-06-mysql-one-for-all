8a1b5c4e18ae53bb2467a1635401ec8a91d7b0cf
William Gomes <williamjog@hotmail.com>
2021-01-13 17:38:49 -0300

    CodeClimate dumb rule 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,1 @@
-CREATE VIEW estatisticas_musicais AS
-SELECT 
-COUNT(DISTINCT can.nome) AS cancoes,
-COUNT(DISTINCT art.nome) AS artistas,
-COUNT(DISTINCT alb.nome) AS album
-FROM cancoes AS can, artista AS art, album AS alb ;
+CREATE VIEW estatisticas_musicais AS SELECT COUNT(DISTINCT can.nome) AS cancoes, COUNT(DISTINCT art.nome) AS artistas, COUNT(DISTINCT alb.nome) AS album FROM cancoes AS can, artista AS art, album AS alb ;
