c134f2d457220704dc368b0afe398fccb707e995
Bruna Franciele <brunafranciele1902@gmail.com>
2021-01-13 19:56:52 -0300

    correção do requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,4 +3,6 @@ SELECT ROUND(MIN(P.price), 2) AS faturamento_minimo,
 ROUND(MAX(P.price), 2) AS faturamento_maximo,
 ROUND(AVG(P.price), 2) AS faturamento_medio,
 ROUND(SUM(P.price), 2) AS faturamento_total
-FROM SpotifyClone.plans AS P;
+FROM SpotifyClone.plans AS P,
+SpotifyClone.users as U
+WHERE U.plan_id = P.plan_id;
