f41e12d0ac633afbcf6ceae7b3e00d7a2c402781
William Gomes <williamjog@hotmail.com>
2021-01-13 16:54:47 -0300

    2nd requirement distinct clause

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT 
-COUNT(can.nome) AS cancoes,
-COUNT(art.nome) AS artistas,
-COUNT(alb.nome) AS album
+COUNT(DISTINCT can.nome) AS cancoes,
+COUNT(DISTINCT art.nome) AS artistas,
+COUNT(DISTINCT alb.nome) AS album
 FROM cancoes AS can, artista AS art, album AS alb ;
