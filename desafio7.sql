d827869112caeb5cbd8b0ad8c2ddfc4ebb5e048d
Luiz Mello <luizmacmillan@gmail.com>
2021-01-13 19:36:58 -0300

    Correção do desafio 7.

--- desafio7.sql
+++ desafio7.sql
@@ -6,5 +6,5 @@ COUNT(s.artista_id) AS seguidores
 FROM artistas ar
 INNER JOIN albums ab ON ar.artista_id = ab.artista_id
 INNER JOIN seguindo_artistas s ON ar.artista_id = s.artista_id
-GROUP BY ab.album
-ORDER BY seguidores DESC, ar.artista ASC, ab.album;
+GROUP BY ar.artista, ab.album
+ORDER BY seguidores DESC, ar.artista ASC, ab.album ASC;
