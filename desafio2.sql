e73580c10edf8749a340b28dd0e8494bccb65c46
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-13 17:26:23 -0300

    Second challenge

---
+++ desafio2.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+
+CREATE VIEW estatisticas_musicais AS
+SELECT
+(
+SELECT COUNT(*) FROM SpotifyClone.cancoes
+) AS cancoes,
+(
+SELECT COUNT(*) FROM SpotifyClone.artistas
+) AS artistas,
+(
+SELECT COUNT(*) FROM SpotifyClone.albums
+) AS albuns;
