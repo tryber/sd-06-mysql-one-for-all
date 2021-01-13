8e6c6292d73b7cca059c04a6b83f8d85ba550d33
William Gomes <williamjog@hotmail.com>
2021-01-13 20:01:40 -0300

    5º e 6º requisito - tentando entender os erros

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT can.nome AS cancao, COUNT(his.cancoes_id) AS reproducoes
-FROM spotifyclone.cancoes AS can
-INNER JOIN spotifyclone.historico_de_reproducoes AS his
+FROM SpotifyClone.cancoes AS can
+INNER JOIN SpotifyClone.historico_de_reproducoes AS his
 ON can.cancoes_id = his.cancoes_id
 GROUP BY cancao
 ORDER BY reproducoes DESC, cancao
