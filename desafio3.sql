2456cb42193f88ce4a1c122c5c91178f4872f2b8
pa-volpin <paulo.volpin@gmail.com>
2021-01-13 18:12:47 -0300

    Task 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
+
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT u.nome AS usuario, c.nome AS nome
+FROM reproducao AS r
+INNER JOIN usuario AS u ON r.usuario_id = u.usuario_id
+INNER JOIN cancao AS c ON r.cancao_id = c.cancao_id
+ORDER BY u.nome, c.nome;
+
+SELECT * FROM historico_reproducao_usuarios;
