358ae9d9f13fce26f7af7f41f30752eaaa59d15f
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-14 15:07:15 -0300

    i got the correct result the first time, anyway making some changes to test

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
 USE SpotifyClone;
 
-DROP VIEW IF EXISTS estatisticas_musicais;
-
 CREATE VIEW estatisticas_musicais AS
 SELECT
-(SELECT COUNT(song) FROM SpotifyClone.songs) AS 'cancoes',
-(SELECT COUNT(artist_name) FROM SpotifyClone.artists) AS 'artistas',
-(SELECT COUNT(album_name) FROM SpotifyClone.albums) AS 'albuns';
+(SELECT COUNT(song_id) FROM SpotifyClone.songs) AS 'cancoes',
+(SELECT COUNT(artist_id) FROM SpotifyClone.artists) AS 'artistas',
+(SELECT COUNT(album_id) FROM SpotifyClone.albums) AS 'albuns';
