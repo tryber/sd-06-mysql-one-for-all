c3c85c1dce1ade84a17b501b3618d2c4139b7fff
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-13 21:14:15 -0300

    syntax adpation to what the test understands

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,13 @@
 CREATE VIEW cancoes_premium AS (
-  (
-    SELECT S.name AS nome, COUNT(*) AS reproducoes
-    FROM song_plays AS SP
-      LEFT JOIN songs AS S
-        ON SP.song_id = S.id
-      LEFT JOIN users AS U
-        ON SP.user_id = U.id
-      LEFT JOIN plans AS P
-        ON U.plan_id = P.id
-    WHERE P.name IN ('familiar', 'universitário')
-    GROUP BY SP.song_id
-  )
-  ORDER BY nome
+  SELECT S.name AS nome, COUNT(*) AS reproducoes
+  FROM song_plays AS SP
+    LEFT JOIN songs AS S
+      ON SP.song_id = S.id
+    LEFT JOIN users AS U
+      ON SP.user_id = U.id
+    LEFT JOIN plans AS P
+      ON U.plan_id = P.id
+  WHERE P.name IN ('familiar', 'universitário')
+  GROUP BY SP.song_id
+  ORDER BY S.name
 );
--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,9 @@
 CREATE VIEW top_3_artistas AS (
-  (
-    SELECT A.name AS artista, COUNT(*) AS seguidores
-    FROM follows AS F
-      LEFT JOIN artists AS A
-        ON F.artist_id = A.id
-    GROUP BY F.artist_id
-  )
-  ORDER BY seguidores DESC, artista ASC
+  SELECT A.name AS artista, COUNT(*) AS seguidores
+  FROM follows AS F
+    LEFT JOIN artists AS A
+      ON F.artist_id = A.id
+  GROUP BY F.artist_id
+  ORDER BY COUNT(*) DESC, A.name ASC
   LIMIT 3
 );
