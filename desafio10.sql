59ccc273200090b875a005a2a1b6bf683f859c60
William Gomes <williamjog@hotmail.com>
2021-01-14 05:07:53 -0300

    10º Desafio - arrumando parametro da funcao

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,12 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario VARCHAR(40))	
+CREATE FUNCTION quantidade_musicas_no_historico(id_usuario INT)	
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantidade_de_musicas INT;
-SELECT COUNT(hist.usuario_id)
-FROM spotifyclone.historico_de_reproducoes AS hist
-INNER JOIN spotifyclone.usuario AS uzer
-ON uzer.nome = nome_usuario AND hist.usuario_id = uzer.usuario_id
+SELECT COUNT(usuario_id)
+FROM spotifyclone.historico_de_reproducoes
+WHERE spotifyclone.historico_de_reproducoes.usuario_id = id_usuario
 INTO quantidade_de_musicas;
 RETURN quantidade_de_musicas;
 END $$
