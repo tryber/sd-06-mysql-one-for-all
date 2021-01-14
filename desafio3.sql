565557469ce18638545dc695e0c117820a786c2f
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-14 16:22:43 -0300

    feat: req. 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios
+  AS SELECT
+    u.user_name usuario,
+    s.song_name nome
+  FROM SpotifyClone.History h
+    INNER JOIN SpotifyClone.User u
+      ON h.user_id = u.user_id
+    INNER JOIN SpotifyClone.Songs s
+      ON s.song_id = h.song_id
+  ORDER BY `usuario`, `nome`;
