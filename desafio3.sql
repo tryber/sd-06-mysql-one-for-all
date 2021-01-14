84f6de9bc72ef19d7dba3215e79f12eea74fed37
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-13 23:52:32 -0300

    desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.user_name AS usuário,
+SELECT u.user_name AS usuario,
 s.song_name AS nome
 FROM Users AS u
 INNER JOIN Users_Reprod_History AS urh ON u.user_id = urh.user_id
 INNER JOIN Songs AS s ON urh.song_id = s.song_id
-ORDER BY usuário, nome;
+ORDER BY usuario, nome;
