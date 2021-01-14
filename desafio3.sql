e0ebb34772c784ac7730386524c6ab45353cf12d
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-14 16:49:04 -0300

    fix: table name; style: codeclimate compliance

--- desafio3.sql
+++ desafio3.sql
@@ -3,8 +3,9 @@ CREATE VIEW historico_reproducao_usuarios
     u.user_name usuario,
     s.song_name nome
   FROM SpotifyClone.History h
-    INNER JOIN SpotifyClone.User u
+    INNER JOIN SpotifyClone.Users u
       ON h.user_id = u.user_id
     INNER JOIN SpotifyClone.Songs s
       ON s.song_id = h.song_id
   ORDER BY `usuario`, `nome`;
+  
