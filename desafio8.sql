eead3b3bed005d8493c09813d95d6c6a1fa9772e
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-13 22:08:31 -0300

    tentando entender o erro

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER trigger_usuario_delete AFTER DELETE
 ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico WHERE usuario_id = old.id;
+DELETE FROM historico WHERE usuario_id = usuarios.id;
 END $$
 
 DELIMITER ;
