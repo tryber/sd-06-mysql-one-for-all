3b6f2a48f7f6af2a68d77faae7cd920ab64a226a
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-14 13:39:07 -0300

    desafio3

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 CREATE VIEW estatisticas_musicais AS
-    (SELECT COUNT(s.song_name) AS cancoes
+    (SELECT COUNT(s.song_id) AS cancoes
     FROM Songs AS s)
     UNION
-    (SELECT COUNT(ar.artist_name) AS artistas
+    (SELECT COUNT(ar.artist_id) AS artistas
     FROM Artists AS ar)
     UNION
-    (SELECT COUNT(al.album_name) AS albuns
+    (SELECT COUNT(al.album_id) AS albuns
     FROM Albums AS al);
---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.user_name AS usuario,
+    s.song_name AS nome
+FROM Users AS u
+    INNER JOIN History AS h
+        ON u.user_id = h.user_id
+    INNER JOIN Songs AS s
+        ON s.song_id = h.song_id;
