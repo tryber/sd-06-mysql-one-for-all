efb38e13aacf76e0b8513b8c6c3364c56acee376
isabellavjs <isabellavjs@gmail.com>
2021-01-14 13:31:15 -0300

    Desafio 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,6 @@
 USE SpotifyClone;
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT U.nome_usuario AS usuario, 
-C.nome_cancao AS nome
-FROM usuarios U
-INNER JOIN historico_execucoes HE ON HE.usuario_id = U.usuario_id
-INNER JOIN cancoes C ON C.cancoes_id = HE.cancoes_id
-ORDER BY usuario, nome;
+CREATE VIEW estatisticas_musicais AS
+SELECT 
+(SELECT COUNT(*) FROM SpotifyClone.cancoes) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns;
