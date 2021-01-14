8d792be5a51ca77b5e88817587173180d117eebb
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-14 19:23:26 -0300

    Req.4

--- desafio4.sql
+++ desafio4.sql
@@ -2,9 +2,9 @@ CREATE VIEW top_3_artistas AS
 SELECT a.artist_name 'artista',
 (
   SELECT COUNT(f.user_id)
-  FROM Follow f
+  FROM SpotifyClone.Follow f
   WHERE a.artist_id = f.artist_id 
 ) 'seguidores'
-FROM Artists a
+FROM SpotifyClone.Artists a
 ORDER BY `seguidores` DESC, `artista`
 LIMIT 3;
