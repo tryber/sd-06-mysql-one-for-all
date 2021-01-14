4fe45d8df3639fae23b071c69cb7641e42fa3af7
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-14 00:09:44 -0300

    Req.3]

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
-  SELECT u.user_name 'usuario',
-    s.song_name 'nome'
-  FROM History h
-    INNER JOIN Users u 
-      ON u.user_id = h.user_id
-    INNER JOIN Songs s
-  	  ON h.song_id = s.song_id
-  ORDER BY u.user_name, s.song_name;
+SELECT u.user_name 'usuario',
+s.song_name 'nome'
+FROM History h
+INNER JOIN Users u 
+ON u.user_id = h.user_id
+INNER JOIN Songs s
+ON h.song_id = s.song_id
+ORDER BY u.user_name, s.song_name;
