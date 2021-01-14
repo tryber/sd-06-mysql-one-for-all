47114a3b7a58d5753bd7f5eb126260f663b138a7
bynho25 <pablo.trybe@gmail.com>
2021-01-14 00:15:05 -0300

    Fix typo

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(song_id) AS cancoes,
-(SELECT COUNT(artist) FROM SpotifyClone.artists) AS artistsa,
+(SELECT COUNT(artist) FROM SpotifyClone.artists) AS artistas
 (SELECT COUNT(album) FROM SpotifyClone.albums) AS albuns FROM SpotifyClone.songs;
