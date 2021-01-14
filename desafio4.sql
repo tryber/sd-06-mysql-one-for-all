95db43a8162059f24510e7989b0ce487fc140500
isabellavjs <isabellavjs@gmail.com>
2021-01-14 14:09:58 -0300

    Corrige nome coluna

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 CREATE VIEW top_3_artistas AS
-SELECT A.nome_artista AS artistas,
+SELECT A.nome_artista AS artista,
 COUNT(SE.usuario_id) AS seguidores
 FROM SpotifyClone.seguindo_artistas SE
 INNER JOIN SpotifyClone.artistas A ON A.artista_id = SE.artista_id
-GROUP BY artistas
-ORDER BY seguidores DESC, artistas
+GROUP BY artista
+ORDER BY seguidores DESC, artista
 LIMIT 3;
