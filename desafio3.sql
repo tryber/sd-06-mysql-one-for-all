1bf126a5773d227872e647ee9beec7ec57e8f0ad
Forna <fornagieri.artur@gmail.com>
2021-01-14 13:11:52 -0300

    fix desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,4 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT U.nome AS 'usuario', C.nome AS 'nome'
 FROM usuario AS U, cancoes AS C, historico_de_reproducoes AS HR
 WHERE U.usuario_id = HR.usuario_id AND C.cancoes_id = HR.cancoes_id
-ORDER BY `usuario`, `cancoes`;
+ORDER BY `usuario`, `nome`;
