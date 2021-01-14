e6cf40de71e4078f575488e2a6b3c4d638e2f69e
isabellavjs <isabellavjs@gmail.com>
2021-01-14 13:58:27 -0300

    Adiciona schema.

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 CREATE VIEW top_3_artistas AS
 SELECT A.nome_artista AS artistas,
 COUNT(SE.usuario_id) AS seguidores
-FROM seguindo_artistas SE 
-INNER JOIN artistas A ON A.artista_id = SE.artista_id
+FROM SpotifyClone.seguindo_artistas SE
+INNER JOIN SpotifyClone.artistas A ON A.artista_id = SE.artista_id
 GROUP BY artistas
 ORDER BY seguidores DESC, artistas
 LIMIT 3;
