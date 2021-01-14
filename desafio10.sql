a3edd370d7cf1cf5ec03ac299e8b5f4d52ac6c4d
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-14 08:40:10 -0300

    :bug: Desafio 10 (Correçao 01)

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(20))
+CREATE FUNCTION quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE total_musicas INT;
-    SELECT COUNT(*)
-    FROM SpotifyClone.historico AS a
-    JOIN SpotifyClone.usuarios AS b
-    ON a.usuario = b.usuario_id
-    WHERE b.nome = nome INTO total_musicas;
+    SELECT COUNT(musica)
+    FROM SpotifyClone.historico
+    WHERE usuario = id INTO total_musicas;
     RETURN total_musicas;
 END $$
 
