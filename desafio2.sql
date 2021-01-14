bce6f9e9e83a8404cb444192c6c1c77e45ff08f3
isabellavjs <isabellavjs@gmail.com>
2021-01-14 13:06:19 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+USE SpotifyClone;
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT U.nome_usuario AS usuario, 
+C.nome_cancao AS nome
+FROM usuarios U
+INNER JOIN historico_execucoes HE ON HE.usuario_id = U.usuario_id
+INNER JOIN cancoes C ON C.cancoes_id = HE.cancoes_id
+ORDER BY usuario, nome;
