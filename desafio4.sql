57ddfe199b2eaa02b972b76ebe2835591cc9aa1d
Kesley Lahoz <namielahoz@gmail.com>
2021-01-14 10:28:37 -0300

    desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ DROP VIEW IF EXISTS `top_3_artistas`;
 CREATE VIEW `top_3_artistas` AS
 SELECT
 A.nome_artista AS 'artista',
-COUNT(S.seguidores) AS 'seguidores'
+COUNT(S.usuario_id) AS 'seguidores'
 FROM SpotifyClone.artistas AS A
 JOIN SpotifyClone.seguindo_artista AS S ON A.artista_id = S.artista_id
 GROUP BY A.artista_id
