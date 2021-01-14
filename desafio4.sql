f882eba2e2d1b36b4284d80075f53bfdaf919497
isabellavjs <isabellavjs@gmail.com>
2021-01-14 13:41:46 -0300

    Adiciona desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+USE SpotifyClone;
+CREATE VIEW top_3_artistas AS
+SELECT A.nome_artista AS artistas,
+COUNT(SE.usuario_id) AS seguidores
+FROM seguindo_artistas SE 
+INNER JOIN artistas A ON A.artista_id = SE.artista_id
+GROUP BY artistas
+ORDER BY seguidores DESC, artistas
+LIMIT 3;
