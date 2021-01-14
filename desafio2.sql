ee165422e99d46c04a8018fc62c0fb5d1f46176e
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-14 13:32:08 -0300

    Fix desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 CREATE VIEW estatisticas_musicais AS
-    SELECT COUNT(s.song_name) AS cancoes
-    FROM Songs AS s
+    (SELECT COUNT(s.song_name) AS cancoes
+    FROM Songs AS s)
     UNION
-    SELECT COUNT(ar.artist_name) AS artistas
-    FROM Artists AS ar
+    (SELECT COUNT(ar.artist_name) AS artistas
+    FROM Artists AS ar)
     UNION
-    SELECT COUNT(al.album_name) AS albuns
-    FROM Albums AS al;
+    (SELECT COUNT(al.album_name) AS albuns
+    FROM Albums AS al);
