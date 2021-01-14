2e9aff01393c57d8a76fe7f646cb8246dd8b9553
Renan Cunha <renan2610@gmail.com>
2021-01-14 18:15:43 -0300

    Adding challenge 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+ROUND(MIN(p.valor_plano), 2) AS 'faturamento_minimo',
+ROUND(MAX(p.valor_plano), 2) AS 'faturamento_maximo',
+ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
+ROUND(SUM(p.valor_plano), 2) AS 'faturamento_total'
+FROM usuarios AS u
+INNER JOIN planos AS p ON p.plano_id = u.plano_id;
