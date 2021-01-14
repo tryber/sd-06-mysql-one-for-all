df11114c07f13524ed251ecf2dfa14422275c700
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-14 20:10:44 -0300

    Req.6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT ROUND(MIN(s.subscription_cost), 2) 'faturamento_minimo',
-   ROUND(MAX(s.subscription_cost), 2) 'faturamento_maximo',
-   ROUND(AVG(s.subscription_cost), 2) 'faturamento_medio',
-   ROUND(SUM(s.subscription_cost), 2) 'faturamento_total'
+  ROUND(MAX(s.subscription_cost), 2) 'faturamento_maximo',
+  ROUND(AVG(s.subscription_cost), 2) 'faturamento_medio',
+  ROUND(SUM(s.subscription_cost), 2) 'faturamento_total'
 FROM Users u
   INNER JOIN Subscriptions s
     ON u.subscription_id = s.subscription_id;
