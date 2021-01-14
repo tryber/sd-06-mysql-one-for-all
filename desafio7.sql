d1bc23bc0112fe7abc1858280db7989c1a6bd3e7
Renan Cunha <renan2610@gmail.com>
2021-01-14 18:49:43 -0300

    Adding challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+art.artista AS 'artista',
+alb.album AS `album`,
+(
+SELECT COUNT(*) FROM artistas_seguidos AS a
+WHERE a.artista_id = art.artista_id
+) AS `seguidores`
+FROM artistas AS art
+INNER JOIN albuns AS alb ON art.artista_id = alb.artista_id;
