a07ebbe2aa8a8ed4af0c955df1777ac5a6d9df13
Daniel Jesus <dev.danieljs@gmail.com>
2021-01-14 19:30:16 -0300

    feat: add challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+CREATE VIEW SpotifyClone.perfil_artistas AS
+    SELECT 
+        ar.artista, al.album, COUNT(se.id_artista) AS 'seguidores'
+    FROM
+        artistas ar
+            INNER JOIN
+        albuns al ON ar.id_artista = al.id_artista
+            INNER JOIN
+        seguindo se ON ar.id_artista = se.id_artista
+    GROUP BY al.id_album
+    ORDER BY `seguidores` DESC , ar.artista , al.album;
