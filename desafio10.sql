cb25025438af998d4f8e3c41def3315e1a7a41ee
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-01-14 11:20:11 -0300

    Depois que vi que era pelo ID e não pelo nome, mais fácil do que eu pensei. Isso que dá não ler o requisito direito

--- desafio10.sql
+++ desafio10.sql
@@ -1,20 +1,17 @@
+USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(cliente VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE resultado INT;
 SELECT
-COUNT(can.cancoes_id)
-FROM SpotifyClone.historico
-INNER JOIN SpotifyClone.usuario AS us
-ON us.usuario_id = historico.usuario_id
-INNER JOIN SpotifyClone.cancoes AS can
-ON can.cancoes_id = historico.cancoes_id
-WHERE us.usuario_nm = cliente INTO resultado;
+COUNT(user_id)
+FROM SpotifyClone.historico AS his
+WHERE his.usuario_id = user_id INTO resultado;
 RETURN resultado;
 END $$
 
 DELIMITER ;
 
-SELECT quantidade_musicas_no_historico("Bill");
+SELECT quantidade_musicas_no_historico(3);
