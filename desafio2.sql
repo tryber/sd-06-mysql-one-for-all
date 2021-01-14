e661dbed4f34c74a7282a1b6e20b969f0bb64fe4
DekoPossas <andrepossaster@gmail.com>
2021-01-14 16:03:03 -0300

    primeiro desafio

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(DISTINCT canc.cancao) AS cancoes,
+COUNT(DISTINCT art.artista) AS artistas,
+COUNT(DISTINCT alb.album) AS albuns
+FROM SpotifyClone.albuns AS alb, SpotifyClone.cancoes AS canc, SpotifyClone.artistas AS art;
