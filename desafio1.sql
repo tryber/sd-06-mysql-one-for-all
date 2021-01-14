45e6b2b2461bede81b6927d9bc52e8fc0272cecb
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-13 21:38:05 -0300

    fix: wrong insertions

--- desafio1.sql
+++ desafio1.sql
@@ -92,14 +92,14 @@ VALUES
     ('Time Fireworks', 1, 2),
     ('Magic Circus', 2, 3),
     ('Honey, So Do I', 2, 3),
-    ('Sweetie, Let\'s Go Wild', 2, 3),
+    ("Sweetie, Let's Go Wild", 2, 3),
     ('She Knows', 2, 3),
     ('Fantasy For Me', 3, 4),
     ('Celebration Of More', 3, 4),
     ('Rock His Everything', 3, 4),
     ('Home Forever', 3, 4),
     ('Diamond Power', 3, 4),
-    ('Honey, Let\'s Be Silly', 3, 4),
+    ("Honey, Let's Be Silly", 3, 4),
     ('Thang Of Thunder', 4, 5),
     ('Words Of Her Life', 4, 5),
     ('Without My Streets', 4, 5);
@@ -113,7 +113,7 @@ VALUES
 (2, 3),
 (3, 2),
 (3, 1),
-(4, 3);
+(4, 4);
 
 INSERT INTO History (user_id, song_id)
 VALUES
@@ -122,10 +122,10 @@ VALUES
 (1, 14),
 (1, 16),
 (2, 13),
-(2, 18),
+(2, 17),
 (2, 2),
 (2, 15),
-(3, 15),
+(3, 4),
 (3, 16),
 (3, 6),
 (4, 3),
