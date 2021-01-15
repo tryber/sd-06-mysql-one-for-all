10f5b21582b1086b5ddcfb66480b9cf54e56a669
rafMewtwo <ponciano.raf@gmail.com>
2021-01-15 14:45:39 -0300

    desafiox7.2

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,4 @@
 CREATE VIEW perfil_artistas AS
-;
-
 SELECT t1.artista AS artista,
 t2.album AS album,
 COUNT(t3.usuarios_id)
@@ -10,11 +8,3 @@ ON t1.id = t2.artistas_id
 INNER JOIN usuarios_has_artistas AS t3
 ON t1.id = t3.artistas_id
 GROUP BY t2.album;
-
-SELECT * FROM artistas;
-SELECT * FROM albuns;
-
-SELECT artistas_id,
-COUNT(usuarios_id)
-FROM usuarios_has_artistas
-GROUP BY artistas_id;
