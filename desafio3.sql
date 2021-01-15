29c7f91e91bab77812ebf7fc743b08c393d2c53b
vapatty6848 <vapatty@gmail.com>
2021-01-15 10:52:47 -0300

    correçao

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
- U.nome As 'usuario', 
- C.titulo_cancao AS 'nome' 
+U.nome As 'usuario', 
+C.titulo_cancao AS 'nome' 
 FROM SpotifyClone.historico_reproducao AS HR
 INNER JOIN SpotifyClone.usuario AS U ON U.usuario_id = HR.usuario_id
 INNER JOIN SpotifyClone.cancao AS C ON C.cancao_id = HR.cancao_id
