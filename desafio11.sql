059757907a9088cabbd36bd70176a5c61ed1d036
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-15 02:10:33 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,13 +4,12 @@ USE SpotifyClone;
 
 CREATE VIEW cancoes_premium AS
 SELECT S.song_name AS nome,
-COUNT(H.song_id) AS reproducoes 
+COUNT(U.user_id) AS reproducoes 
 FROM SpotifyClone.users_history_songs AS H
 JOIN SpotifyClone.users AS U ON U.user_id = H.user_id
-JOIN SpotifyClone.plans AS P ON P.plan_id = U.plan_id
 JOIN SpotifyClone.songs AS S ON H.song_id = S.song_id
-WHERE P.plan_id <> 1
-GROUP BY S.song_name
+WHERE U.plan_id <> 1
+GROUP BY H.song_id
 ORDER BY S.song_name;
 
 SELECT * FROM cancoes_premium;
