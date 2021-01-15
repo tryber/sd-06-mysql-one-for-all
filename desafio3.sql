e0057637c1dc8faddda09f19c217ea7bd070c6e7
RenatoCostaDev <renatotrybe@gmail.com>
2021-01-15 09:06:20 -0300

    Requirement-3

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT u.nome AS `usuario`, c.cancao AS `nome`
+FROM SpotifyClone.usuarios as u
+INNER JOIN SpotifyClone.historico_de_reproducoes h on u.id_usuario = h.id_usuario
+INNER JOIN SpotifyClone.cancoes c on h.id_cancao = c.id_cancao
+ORDER BY `usuario`, `nome`;
