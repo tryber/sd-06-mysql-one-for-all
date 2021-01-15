82ad5a609ae130a95ab21ebf218a1f94d55e9218
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-15 01:20:05 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS cancoes_premium;
 
 CREATE VIEW cancoes_premium AS
 SELECT S.song_name AS nome,
-COUNT(S.song_name) AS reproducoes 
+COUNT(S.song_id) AS reproducoes 
 FROM SpotifyClone.users_history_songs AS H
 JOIN SpotifyClone.users AS U ON U.user_id = H.user_id
 JOIN SpotifyClone.plans AS P ON P.plan_id = U.plan_id
