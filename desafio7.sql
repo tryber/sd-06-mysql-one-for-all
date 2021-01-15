b371da2aaecea66aa55dc1b69939586d802ea10e
rafMewtwo <ponciano.raf@gmail.com>
2021-01-15 15:18:48 -0300

    desafiox7.4

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,11 @@
 CREATE VIEW perfil_artistas AS
 SELECT t1.artista AS artista,
 t2.album AS album,
-COUNT(t3.usuarios_id)
+COUNT(t3.usuarios_id) AS seguidores
 FROM artistas AS t1
 INNER JOIN albuns AS t2
 ON t1.id = t2.artistas_id
 INNER JOIN usuarios_has_artistas AS t3
 ON t1.id = t3.artistas_id
-GROUP BY t2.id;
+GROUP BY t2.id
+ORDER BY seguidores DESC, artista;
