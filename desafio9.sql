8d394199073825eda2bfcf079e7d4e6a727dbed1
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-15 00:22:37 -0300

    desafio9

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE albuns_do_artista(Artist VARCHAR(50)) BEGIN
+SELECT
+  artist.artist_name AS artista,
+  album.album_title AS album
+FROM
+  artist
+  INNER JOIN album ON artist.id = album.artist_id
+HAVING
+  artista = Artist
+ORDER BY
+  album
+END;
