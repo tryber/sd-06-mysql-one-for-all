006f1ffa637d7210963e657a20ab85316f688a6b
isabellavjs <isabellavjs@gmail.com>
2021-01-15 19:22:25 -0300

    Adiciona desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+DROP FUNCTION IF EXISTS quantidade_musicas_no_historico;
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE cancoes_historico INT;
+SELECT COUNT(cancoes_id)
+FROM SpotifyClone.historico_execucoes
+GROUP BY usuario_id
+HAVING usuario_id = id INTO cancoes_historico;
+RETURN cancoes_historico;
+END $$
+DELIMITER ; 
+
+SELECT quantidade_musicas_no_historico(3);
