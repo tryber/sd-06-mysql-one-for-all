defa2ec2124e0309535417b3cbfaac578d0ad251
vapatty6848 <vapatty@gmail.com>
2021-01-15 10:05:30 -0300

    add ordenaçao desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT U.nome As 'usuario', C.titulo_cancao AS 'nome' 
+SELECT
+ U.nome As 'usuario', 
+ C.titulo_cancao AS 'nome' 
 FROM SpotifyClone.historico_reproducao AS HR
-INNER JOIN SpotifyClone.usuario AS U
-ON U.usuario_id = HR.usuario_id
-INNER JOIN SpotifyClone.cancao AS C
-ON C.cancao_id = HR.cancao_id
-ORDER BY U.nome;
+INNER JOIN SpotifyClone.usuario AS U ON U.usuario_id = HR.usuario_id
+INNER JOIN SpotifyClone.cancao AS C ON C.cancao_id = HR.cancao_id
+ORDER BY `usuario`, `nome`;
