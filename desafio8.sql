62d85226b15cc0982d42584d40b61256b8e493f0
Kesley Lahoz <namielahoz@gmail.com>
2021-01-15 12:18:26 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
-DROP TRIGGER IF EXISTS trigger_usuario_delete;
-DELIMITER $$
-CREATE TRIGGER trigger_usuario_delete
-    BEFORE DELETE ON usuario
-    FOR EACH ROW
+DELIMITER $
+
+CREATE TRIGGER trigger_usuario_delete BEFORE DELETE
+ON SpotifyClone.usuario
+FOR EACH ROW
 BEGIN
-    DELETE FROM historico_de_reproducao
-    WHERE historico_de_reproducao.usuario_id = OLD.usuario_id;
-    DELETE FROM seguindo_artista WHERE seguindo_artista.usuario_id = OLD.usuario_id;
-END; $$
+DELETE FROM SpotifyClone.historico_reproducao WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artista WHERE usuario_id = OLD.usuario_id;
+END$
+
 DELIMITER ;
