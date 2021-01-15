a807414136f9769aa38fbc01e752fe93f6656a60
RenatoCostaDev <renatotrybe@gmail.com>
2021-01-15 09:10:00 -0300

    fix:Requirement-3

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
+CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.nome AS `usuario`, c.cancao AS `nome`
 FROM SpotifyClone.usuarios as u
 INNER JOIN SpotifyClone.historico_de_reproducoes h on u.id_usuario = h.id_usuario
