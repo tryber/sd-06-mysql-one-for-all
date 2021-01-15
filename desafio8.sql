715e7675ac6c9ad2df95a453cbd3b2dbf6ae1ccf
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-15 17:04:07 -0300

    fix: req. 8 syntax

--- desafio8.sql
+++ desafio8.sql
@@ -3,9 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE
   ON SpotifyClone.Users
   BEGIN
-    DELETE FROM SpotifyClone.History h,
-    WHERE h.user_id = OLD.user_id
-    DELETE FROM SpotifyClone.Follow f
-    WHERE f.user_id = OLD.user_id
+    DELETE FROM SpotifyClone.History,
+    WHERE History.user_id = OLD.user_id
+    DELETE FROM SpotifyClone.Follow
+    WHERE Follow.user_id = OLD.user_id
   END; $$
 DELIMITER ;
