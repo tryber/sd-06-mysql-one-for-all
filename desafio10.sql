41771a25906ff6fc4b02d308397b6381752affcb
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-01-16 17:46:13 -0300

    Fixing incoming parameter on 10th requirement

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,14 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(userName VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(userID INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE history_total INT;
 SELECT
-COUNT(rh.user_id) AS quantidade_musicas_no_historico
+COUNT(rh.user_id)
 FROM Reproduction_History AS rh
 INNER JOIN User AS u
 ON u.user_id = rh.user_id
-WHERE u.user_name LIKE userName
+WHERE u.user_id LIKE userID
 GROUP BY rh.user_id INTO history_total;
 RETURN history_total;
 END $$
