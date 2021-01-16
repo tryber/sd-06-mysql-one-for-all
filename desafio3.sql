c7882a1146b251ea48ac5f6469de8e611a025a57
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-16 16:50:06 -0300

    req01-correcao01

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
-SELECT hr.jobs.JOB_TITLE AS 'Cargo',
-(hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
-FROM hr.jobs
-ORDER BY (hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY), hr.jobs.JOB_TITLE;
+-- SELECT hr.jobs.JOB_TITLE AS 'Cargo',
+-- (hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+-- FROM hr.jobs
+-- ORDER BY (hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY), hr.jobs.JOB_TITLE;
 -- ORDER BY 'Diferença entre salários máximo e mínimo', 'Cargo';
 -- (CASE
 --     WHEN  IS ???? THEN ????
--- desafio6.sql
+++ desafio6.sql
@@ -10,12 +10,12 @@
 -- ORDER BY hr.employees.FIRST_NAME DESC, hr.jobs.JOB_TITLE;
 -- ORDER BY `Nome completo` DESC, `Cargo`;
 
-SELECT CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME) AS 'Nome completo',
-hr.jobs.JOB_TITLE AS 'Cargo',
-hr.job_history.START_DATE AS  'Data de início do cargo',
-hr.departments.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history, hr.jobs, hr.employees, hr.departments
-WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID AND
-hr.job_history.JOB_ID = hr.jobs.JOB_ID AND
-hr.job_history.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
-ORDER BY hr.employees.FIRST_NAME DESC, hr.jobs.JOB_TITLE;
+-- SELECT CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME) AS 'Nome completo',
+-- hr.jobs.JOB_TITLE AS 'Cargo',
+-- hr.job_history.START_DATE AS  'Data de início do cargo',
+-- hr.departments.DEPARTMENT_NAME AS 'Departamento'
+-- FROM hr.job_history, hr.jobs, hr.employees, hr.departments
+-- WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID AND
+-- hr.job_history.JOB_ID = hr.jobs.JOB_ID AND
+-- hr.job_history.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
+-- ORDER BY hr.employees.FIRST_NAME DESC, hr.jobs.JOB_TITLE;
