b24816502bc3279d211d80c3040b3be7fc4a9bcc
danieleperse <danieleperse3@gmail.com>
2021-01-15 21:02:26 -0300

    desafio 7 - alterei query - usando where na subquery

--- desafio7.sql
+++ desafio7.sql
@@ -3,11 +3,9 @@ SELECT
 ar.nome AS 'artista',
 al.nome AS 'album',
 (SELECT
-COUNT(u.usuario_id)
+COUNT(s.usuario_id)
 FROM SpotifyClone.seguindo_artistas AS s
-INNER JOIN SpotifyClone.usuario AS u
-ON s.usuario_id = u.usuario_id
-AND s.artista_id = ar.artista_id
+WHERE s.artista_id = ar.artista_id
 ) AS 'seguidores'
 FROM SpotifyClone.album AS al
 INNER JOIN SpotifyClone.artista AS ar
