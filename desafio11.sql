821ec96d5ecdf0b4d846e6d530a0337c256e7eb1
rafMewtwo <ponciano.raf@gmail.com>
2021-01-15 22:37:14 -0300

    desafiox11.4

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,13 @@
 CREATE VIEW cancoes_premium AS
 SELECT c.cancao AS nome,
-COUNT(u.cancoes_id) AS reproducoes
-FROM cancoes AS c
-INNER JOIN usuarios_has_cancoes AS u
-ON c.id = cancoes_id
+COUNT(uc.cancoes_id) AS reproducoes
+FROM usuarios_has_cancoes AS uc
+INNER JOIN cancoes AS c
+ON c.id = uc.cancoes_id
+INNER JOIN usuarios AS u
+ON uc.usuarios_id = u.id
+INNER JOIN planos AS p
+ON u.planos_id = p.id
+WHERE p.plano = 'familiar' OR p.plano = 'universitario'
 GROUP BY nome
 ORDER BY nome;
