8ec1400bcd8af829f60683d6f9fbae3ac77230a5
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-16 19:49:31 -0300

    req05

--- desafio4.sql
+++ desafio4.sql
@@ -3,12 +3,12 @@ CREATE VIEW top_3_artistas AS
 SELECT
 SpotifyClone.artistas.artistas_nome AS 'artista',
 COUNT(SpotifyClone.seguindo_artistas.usuarios_id) AS `seguidores`
-FROM SpotifyClone.seguindo_artistas, SpotifyClone.artistas,
+FROM SpotifyClone.seguindo_artistas, SpotifyClone.artistas
 
 WHERE
 SpotifyClone.artistas.artistas_id = SpotifyClone.seguindo_artistas.artistas_id
 
+GROUP BY SpotifyClone.artistas.artistas_nome
+
 ORDER BY `seguidores` DESC, SpotifyClone.artistas.artistas_nome
 LIMIT 3;
-
--- correto usar ou tem outra forma?
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,14 @@
--- SELECT hr.jobs.JOB_TITLE AS 'Cargo',
--- ROUND(hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY) AS 'Variação Salarial',
--- ROUND(hr.jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
--- ROUND(hr.jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
--- FROM hr.jobs
--- ORDER BY (hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY), hr.jobs.JOB_TITLE;
--- ORDER BY 'Variação Salarial', 'Cargo';
+DROP VIEW IF EXISTS `top_2_hits_do_momento`;
+CREATE VIEW top_2_hits_do_momento AS
+SELECT
+SpotifyClone.musicas.musicas_titulo AS 'cancao',
+COUNT(SpotifyClone.historicos.usuarios_id) AS `reproducoes`
+FROM SpotifyClone.musicas, SpotifyClone.historicos
+
+WHERE
+SpotifyClone.musicas.musicas_id = SpotifyClone.historicos.musicas_id
+
+GROUP BY SpotifyClone.musicas.musicas_titulo
+
+ORDER BY `reproducoes` DESC, SpotifyClone.musicas.musicas_titulo
+LIMIT 2;
