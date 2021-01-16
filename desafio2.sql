2dae460500839d0674d4a6873bb9a2b248801da5
Marxneves <marxkhristie@gmail.com>
2021-01-15 21:04:32 -0300

    corrigindo desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,10 @@
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais 
 AS SELECT 
 COUNT( s.song_id) AS `cancoes`,
 COUNT(DISTINCT a.artist_id) AS `artistas`,
 COUNT(DISTINCT al.album_id) AS `albuns`
-FROM Songs AS s
-INNER JOIN Artists AS a
+FROM SpotifyClone.Songs AS s
+INNER JOIN SpotifyClone.Artists AS a
 ON s.artist_id = a.artist_id
-INNER JOIN Albums as al
+INNER JOIN SpotifyClone.Albums as al
 ON s.album_id =al.album_id;
