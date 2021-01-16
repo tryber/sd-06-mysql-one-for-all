4807cd1b532c3a77e32605503cfc3303eb1dcffb
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-16 20:52:13 -0300

    req07

--- desafio7.sql
+++ desafio7.sql
@@ -1,16 +1,18 @@
--- SELECT UCASE(CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME)) AS 'Nome completo',
--- hr.job_history.START_DATE AS  'Data de início',
--- hr.employees.SALARY AS 'Salário'
--- FROM hr.employees
--- INNER JOIN hr.job_history ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
--- WHERE MONTH(hr.job_history.START_DATE) IN(1,2,3,01,02,03)
--- ORDER BY hr.employees.FIRST_NAME, hr.job_history.START_DATE;
+DROP VIEW IF EXISTS `perfil_artistas`;
+CREATE VIEW perfil_artistas AS
+SELECT
+SpotifyClone.artistas.artistas_nome AS 'artista',
+SpotifyClone.albuns.albuns_titulo AS 'album',
+COUNT(SpotifyClone.seguindo_artistas.usuarios_id) AS `seguidores`
 
+FROM SpotifyClone.artistas, SpotifyClone.albuns, SpotifyClone.seguindo_artistas
 
--- teste SELF JOIN
--- SELECT UPPER(CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME)) AS 'Nome completo',
--- hr.job_history.START_DATE AS  'Data de início',
--- hr.employees.SALARY AS 'Salário',
--- FROM hr.employees, hr.job_history
--- WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
--- ORDER BY hr.employees.FIRST_NAME, hr.job_history.START_DATE;
+WHERE
+SpotifyClone.artistas.artistas_id = SpotifyClone.seguindo_artistas.artistas_id
+AND
+SpotifyClone.artistas.artistas_id = SpotifyClone.albuns.artistas_id
+
+GROUP BY SpotifyClone.artistas.artistas_nome, SpotifyClone.albuns.albuns_titulo
+
+ORDER BY `seguidores` DESC, SpotifyClone.artistas.artistas_nome, SpotifyClone.albuns.albuns_titulo;
+
