0c30e556e11b40ba6e7e22c3c4c09dc55c2c98ea
LeonardoCavachini <cavachini240@gmail.com>
2021-01-15 22:20:21 -0300

    desafio2-6

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT COUNT(c.id) AS cancoes, COUNT(DISTINCT a.artista_id) AS artistas, COUNT(DISTINCT c.album_id) AS albuns
+FROM cancoes AS c
+INNER JOIN album AS a
+ON c.album_id = a.id;
---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.nome AS `usuario`,  c.nome AS `nome`
+FROM usuario AS u
+INNER JOIN historico AS h
+ON u.id = h.usuario_id
+INNER JOIN cancoes AS c
+ON c.id = h.cancoes_id	
+ORDER BY `usuario`, `nome`;
