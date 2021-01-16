b7396a5b01ecbf93f7c2b06f5fcc37f7006e6fc8
higoramorim <1810042@aluno.univesp.br>
2021-01-16 17:22:49 -0300

    des 1 17:22

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+create view top_3_artistas AS
+  select a.artista as artista,
+  count(SA.usuario_id) as seguidores
+  from SpotifyClone.artistas as a
+  join SpotifyClone.seguindo_artistas as SA on a.artista_id = SA.artista_id
+  group by a.artista_id
+  order by seguidores desc, artista
+  limit 3;
+  
