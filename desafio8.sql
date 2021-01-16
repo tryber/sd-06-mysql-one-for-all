5d874e9d04831834cdd7efb797fee55ee09346e3
bynho25 <pablo.trybe@gmail.com>
2021-01-15 21:56:37 -0300

    Requirements 7 to 9

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.followers WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.reproduction_history WHERE user_id = OLD.user_id;
+END $$
+DELIMITER ;
