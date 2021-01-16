ee001273f01d4bda84950f8c4917f44d54cee444
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-16 12:15:35 -0300

    CODE CLIMATE

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(45))
 BEGIN
-	SELECT a.artist_name AS artista,
-	b.album_name AS album
-    FROM Artists AS a
-    INNER JOIN Albuns AS b
-    ON b.artist_id = a.artist_id
-    WHERE a.artist_name = nome_artista
-    ORDER BY album;
+SELECT a.artist_name AS artista,
+b.album_name AS album
+FROM Artists AS a
+INNER JOIN Albuns AS b
+ON b.artist_id = a.artist_id
+WHERE a.artist_name = nome_artista
+ORDER BY album;
 END $$
 DELIMITER ;
