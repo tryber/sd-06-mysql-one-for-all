21bb06a24d4d178e48019085ad3f74b7e548f8a8
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-16 12:48:44 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON Users
+FOR EACH ROW
+BEGIN
+DELETE FROM Users_Reprod_History
+WHERE OLD.user_id = user_id;
+DELETE FROM Users_Following_Artists
+WHERE OLD.user_id = user_id;
+END $$
+DELIMITER ;
