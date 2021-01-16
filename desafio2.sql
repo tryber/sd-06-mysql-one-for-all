72629ea34f98782da7afc3f51b9c603f1057df2e
higoramorim <1810042@aluno.univesp.br>
2021-01-16 17:29:17 -0300

    des 1 17:29

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,7 @@
-CREATE VIEW estatisticas_musicais AS
+  CREATE VIEW estatisticas_musicais AS
   select count(distinct c.cancoes_id) as cancoes,
   count(distinct a.artista) as artistas,
   count(distinct al.album) as albuns
   from SpotifyClone.cancoes as c
   inner join SpotifyClone.artistas as a
   inner join SpotifyClone.albums as al;
- 
