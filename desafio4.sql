e13e4f2503986491ba425cd81f7b0adf407f5699
higoramorim <1810042@aluno.univesp.br>
2021-01-17 08:57:48 -0300

    des 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,9 @@
   create view top_3_artistas as
 select a.artista as artista,
 count(sa.usuario_id) as seguidores
-from SpotifyClone.artistas as a
-inner join SpotifyClone.seguindo_artistas as sa on a.artista_id = sa.artista_id
-group by a.artista_id
+from SpotifyClone.seguindo_artistas as seguidor
+inner join SpotifyClone.usuarios as u on seguidor.usuario_id = u.usuario_id
+inner join SpotifyClone.artistas as a on a.artista_id = seguidor.artista_id
+group by `artista`
 order by `seguidores` desc, `artista`
 limit 3;
