39edfeed8593062bf07d81eee90724b975fa611d
Forna <fornagieri.artur@gmail.com>
2021-01-17 10:11:02 -0300

    desafio11 fix(3)

--- desafio11.sql
+++ desafio11.sql
@@ -2,12 +2,12 @@ DROP VIEW IF EXISTS cancoes_premium;
 
 CREATE VIEW cancoes_premium AS
 
-SELECT C.nome AS nome,
-COUNT(HR.cancoes_id) AS reproducoes
+SELECT C.nome AS 'nome',
+COUNT(HR.cancoes_id) AS 'reproducoes'
 FROM historico_de_reproducoes AS HR
 JOIN usuario AS U ON U.usuario_id = HR.usuario_id
 JOIN plano AS P ON P.plano_id = U.plano_id
 JOIN cancoes AS C ON HR.cancoes_id = C.cancoes_id
-WHERE P.plano_id <> 1
+WHERE P.nome IN ('familiar', 'universitário')
 GROUP BY HR.cancoes_id
-ORDER BY C.nome;
+ORDER BY C.nome
