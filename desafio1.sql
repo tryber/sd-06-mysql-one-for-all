3f9860ee41c524389f5acda157d8b5eb46cde750
PAULO LINS <paulolins100@gmail.com>
2021-01-16 21:20:06 -0300

    desafio1fixagain

--- desafio1.sql
+++ desafio1.sql
@@ -47,15 +47,15 @@ INSERT INTO plano(plano_name, plano_valor)
 VALUES
   ('gratuito', 0), ('familiar', 7.99), ('universitário', 5.99);
   INSERT INTO usuario(usuario_name, idade,plano_id) VALUES
-('Thati', 23, 1),
-('Cintia', 35, 3),
-('Bill', 20, 2),
-('Roger', 45, 1);
+  ('Thati', 23, 1),
+  ('Cintia', 35, 3),
+  ('Bill', 20, 2),
+  ('Roger', 45, 1);
 INSERT INTO artista(artista_name) VALUES 
-('Walter Phoenix'),
-('Peter Strong'),
-('Lance Day'),
-('Freedie Shannon');
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
 INSERT INTO album(album_name, artista_id)
 VALUES
   ('Envious', 1),
@@ -65,24 +65,24 @@ VALUES
   ('Temporary Culture', 4);
   INSERT INTO cancoes(cancoes_name, album_id)
 VALUES 
-('Soul For Us', 1),
-('Reflections Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So Do I', 3),
-("Sweetie, Let's Go Wild", 3),
-('She Knows', 3),
-('Fantasy For Me', 4),
-('Celebration Of More', 4),
-('Rock His Everything', 4),
-('Home Forever', 4),
-('Diamond Power', 4),
-("Honey, Let's Be Silly", 4),
-('Thang Of Thunder', 5),
-('Words Of Her Life', 5),
-('Without My Streets', 5);
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
 INSERT INTO historico(usuario_id, cancoes_id)
 VALUES
   (1, 1),
@@ -110,4 +110,3 @@ VALUES
   (3, 1),
   (4, 4);
  
-  
