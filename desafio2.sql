842c1ff93cc11c3b990a91ee7d13bd54599bc7b7
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-17 11:20:55 -0300

    req2 - not  calling the table

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW `estatisticas_musicais` AS (
-SELECT COUNT(spotifyclone.songstable.song_id) AS `cancoes`,
-COUNT( distinct spotifyclone.artiststable.artist_id) AS `artistas`,
-COUNT( distinct spotifyclone.albunstable.album_id) AS `albuns`
-from spotifyclone.songstable, spotifyclone.artiststable, spotifyclone.albunstable );
+SELECT COUNT(distinct song_id) AS `cancoes`,
+COUNT( distinct artiststable.artist_id) AS `artistas`,
+COUNT( distinct albunstable.album_id) AS `albuns`
+from songstable, artiststable, albunstable );
