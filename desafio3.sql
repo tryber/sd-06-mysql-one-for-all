793c8331bbef7cd72d3ea13e1b36a7154a5dcc2d
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-17 12:17:23 -0300

    req3 - de primeira? claro que não

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW `historico_reproducao_usuarios` AS
 SELECT userstable.user_name AS `usuario`,
 songstable.song_name AS `nome`
-FROM ((spotifyclone.reproductiontable
-INNER JOIN spotifyclone.songstable
+FROM ((reproductiontable
+INNER JOIN songstable
 ON reproductiontable.song_id = songstable.song_id)
-INNER JOIN spotifyclone.userstable
+INNER JOIN userstable
 ON reproductiontable.user_id = userstable.user_id)
 ORDER BY `usuario` ASC, `nome` ASC;
