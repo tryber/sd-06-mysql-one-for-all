45916a728eaaf4bdf5e7a146fa40509a0ec1c0d8
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-01-17 12:34:00 -0300

    req 4/5/6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-ROUND(MIN(s.subscription_cost),2) AS `faturamento_minimo`,
-ROUND(MAX(s.subscription_cost),2) AS `faturamento_maximo`,
-ROUND(AVG(s.subscription_cost),2) AS `faturamento_medio`,
-ROUND(SUM(s.subscription_cost),2) AS `faturamento_total`
-FROM SpotifyClone.subscriptions AS s
-INNER JOIN SpotifyClone.users AS u
-ON u.plan_id = s.plan_id;
+ROUND(MIN(SpotifyClone.subscriptions.subscription_cost),2) AS `faturamento_minimo`,
+ROUND(MAX(SpotifyClone.subscriptions.subscription_cost),2) AS `faturamento_maximo`,
+ROUND(AVG(SpotifyClone.subscriptions.subscription_cost),2) AS `faturamento_medio`,
+ROUND(SUM(SpotifyClone.subscriptions.subscription_cost),2) AS `faturamento_total`
+FROM SpotifyClone.users
+INNER JOIN  SpotifyClone.subscriptions
+ON SpotifyClone.users.plan_id = SpotifyClone.subscriptions.plan_id;
