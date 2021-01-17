62832876ffc2e45a531ad7f18599d6b0704e3669
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-16 22:40:41 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM usuario WHERE usuario_id = OLD.usuario_id;
+DELETE FROM seguidores WHERE usuario_id = OLD.usuario_id;
+DELETE FROM reproducoes WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
