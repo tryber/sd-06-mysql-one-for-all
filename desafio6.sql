4e9268f6af7853d28cf8825b61382a03c53c8a82
HCaetano <hrcgeo@gmail.com>
2021-01-17 10:07:28 -0300

    Try old formula for average profit in challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,6 +2,6 @@ CREATE VIEW faturamento_atual AS
 SELECT
 ROUND(MIN(valor_plano), 2) AS faturamento_minimo,
 ROUND(MAX(valor_plano), 2) AS faturamento_maximo,
-ROUND(AVG(valor_plano), 2) AS faturamento_medio,
+ROUND(SUM(valor_plano)/4, 2) AS faturamento_medio,
 ROUND(SUM(valor_plano), 2) AS faturamento_total
 FROM planos;
