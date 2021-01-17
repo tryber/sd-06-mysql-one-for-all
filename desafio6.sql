5e8ab8e81e4443818ae61b70934380abcb407b55
HCaetano <hrcgeo@gmail.com>
2021-01-17 10:01:37 -0300

    Try using ROUND on all values of challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(valor_plano) AS faturamento_minimo,
-MAX(valor_plano) AS faturamento_maximo,
-ROUND(SUM(valor_plano)/4, 2) AS faturamento_medio,
-SUM(valor_plano) AS faturamento_total
+ROUND(MIN(valor_plano), 2) AS faturamento_minimo,
+ROUND(MAX(valor_plano), 2) AS faturamento_maximo,
+ROUND(AVG(valor_plano), 2) AS faturamento_medio,
+ROUND(SUM(valor_plano), 2) AS faturamento_total
 FROM planos;
