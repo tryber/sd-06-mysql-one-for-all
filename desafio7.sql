c23f0e67545dfbcecdce331594052b27a64f1b3f
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-17 11:04:16 -0300

    feat: artists profile

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+ar.name AS artista,
+al.name AS album,
+COUNT(f.artist_id) AS seguidores
+FROM artists AS ar
+INNER JOIN albums AS al
+ON ar.artist_id = al.artist_id
+INNER JOIN followers AS f
+ON f.artist_id = ar.artist_id
+GROUP BY album
+ORDER BY seguidores DESC, artista, album ASC;
