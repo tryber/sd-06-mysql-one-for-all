dd1ea41f7a46358df03301f558dc444976e1826d
higoramorim <1810042@aluno.univesp.br>
2021-01-16 22:49:06 -0300

    des 3 22:49

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
   create view historico_reproducao_usuarios as
-  select n.nome as usuario, c.cancao as nome
-  from SpotifyClone.historico_de_reproducoes as HR
-  inner join SpotifyClone.cancoes as c on HR.cancoes_id = c.cancoes_id
-  inner join SpotifyClone.usuarios as n on HR.usuario_id = n.usuario_id
-  order by usuario asc, nome asc;
+select n.nome as usuario, c.cancao as nome
+from SpotifyClone.historico_de_reproducoes as HR
+inner join SpotifyClone.usuarios as n on n.usuario_id = HR.usuario_id
+inner join SpotifyClone.cancoes as c on c.cancoes_id = HR.cancoes_id
+order by usuario asc, nome asc;
