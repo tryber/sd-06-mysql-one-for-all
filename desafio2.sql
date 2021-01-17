806fdc55238fd3cc06647063ceb2a21c44235a7e
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-16 21:52:41 -0300

    fix: sintax

--- desafio2.sql
+++ desafio2.sql
@@ -4,6 +4,6 @@ CREATE VIEW estatisticas_musicais AS
 SELECT
 (SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.artists) AS artistas,
-(SELECT COUNT(*) FROM SpotifyClone.albums) AS albuns;
+COUNT(*) AS albuns FROM SpotifyClone.albums;
 
 SELECT * FROM estatisticas_musicais;
