2187752531c5bc2f430a7a890d336664c1d0b8b0
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-01-17 10:53:53 -0300

    req 2-try with join

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,7 @@
 CREATE VIEW estatisticas_musicais
-SELECT COUNT(DISTINCT SpotifyClone.musics.music_name) AS cancoes,
-COUNT(DISTINCT SpotifyClone.artists.artist_name) AS artistas,
-COUNT(DISTINCT SpotifyClone.albuns.album_name) AS albuns
-FROM SpotifyClone.musics, SpotifyClone.artists, SpotifyClone.albuns;
+SELECT COUNT(DISTINCT m.music_name) AS cancoes,
+COUNT(DISTINCT a.artist_name) AS artistas,
+COUNT(DISTINCT al.album_name) AS albuns
+FROM SpotifyClone.musics AS 'm'
+JOIN SpotifyClone.artists AS 'a'
+JOIN SpotifyClone.albuns AS 'al';
