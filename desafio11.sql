5595bb38ffa2367ec1099b094bb9cd2d97353fc6
higoramorim <1810042@aluno.univesp.br>
2021-01-17 08:41:54 -0300

    des 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+create view cancoes_premium as
+select c.cancao as nome, count(hr.usuario_id) as reproducoes
+from SpotifyClone.historico_de_reproducoes as hr
+inner join SpotifyClone.cancoes as c on hr.cancoes_id = c.cancoes_id
+inner join SpotifyClone.usuarios as us on hr.usuarios_id = us.usuarios_id
+where us.financeiro_id between 2 and 3
+group by hr.cancoes_id
+order by `nome`;
