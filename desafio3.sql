fb289f18c50a34f6007015fdd74e2461279c430b
labelle <beelsdelonge@gmail.com>
2021-01-16 22:06:38 -0300

    Desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -2,7 +2,7 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT U.usuario_nome AS usuario, M.nome AS nome
 FROM historico as H
 INNER JOIN usuarios AS U
-ON U.usuario_id = H.usuario_id
+ON H.usuario_id = U.usuario_id
 INNER JOIN musicas AS M
-ON M.musica_id = H.musica_id
+ON H.musica_id = M.musica_id
 ORDER BY usuario, nome;
