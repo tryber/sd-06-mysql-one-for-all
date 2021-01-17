3c862933a65ecdfdf258d183dde0ca52527f36ce
higoramorim <1810042@aluno.univesp.br>
2021-01-16 23:20:04 -0300

    des 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,10 +6,10 @@ DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.seguindo_artistas
-WHERE usuario_id = old.usuario_id;
 DELETE FROM SpotifyClone.historico_reproducao_usuarios
-WHERE usuario_id = old.usuario_id;
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
