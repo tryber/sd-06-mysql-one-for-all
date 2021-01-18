480f30ecf3a2ed2e6342f9702f5612678dce36a3
isabellavjs <isabellavjs@gmail.com>
2021-01-18 18:18:40 -0300

    Corrige funçao

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,3 @@
-DROP FUNCTION IF EXISTS quantidade_musicas_no_historico;
 DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
@@ -10,6 +9,4 @@ GROUP BY usuario_id
 HAVING SpotifyClone.historico_execucoes.usuario_id = id INTO cancoes_historico;
 RETURN cancoes_historico;
 END $$
-DELIMITER ; 
-
-SELECT quantidade_musicas_no_historico(3);
+DELIMITER ;
