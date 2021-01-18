542fce41b3e344f8a16e145b5e82cea60b5071e4
Igor-leao <igorleao.eng@gmail.com>
2021-01-18 14:40:59 -0300

    desafio 1 tabelas e colunas

--- desafio1.sql
+++ desafio1.sql
@@ -2,129 +2,164 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
-CREATE TABLE SpotifyClone.artists(
-  artist_id INT PRIMARY KEY,
-  artist_name VARCHAR(50) NOT NULL
-) ENGINE = Innodb;
+USE SpotifyClone;
 
-INSERT INTO SpotifyClone.artists (artist_id, artist_name)
-VALUES
-  (1, 'Freedie Shannon'),
-  (2, 'Lance Day'),
-  (3, 'Peter Strong'),
-  (4, 'Walter Phoenix');
+CREATE TABLE plans(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  price DECIMAL(20, 2) NOT NULL,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW(),
+  CHECK (price >= 0)
+) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.subscriptions(
-  id INT PRIMARY KEY,
-  subscriptions VARCHAR(50) NOT NULL,
-  subscriptions_value FLOAT
-) ENGINE = Innodb;
-
-INSERT INTO SpotifyClone.subscriptions (subscriptions_id, subscriptions, subscriptions_value)
-VALUES
-  (1, 'gratuito', 0),
-  (2, 'familiar', 7.99),
-  (3, 'universitário', 5.99);
-
-CREATE TABLE SpotifyClone.users(
-  user_id INT PRIMARY KEY,
-  user_name VARCHAR(100) NOT NULL,
+CREATE TABLE users(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   age INT NOT NULL,
-  subscriptions_id INT NOT NULL,
-  FOREIGN KEY (subscriptions_id) REFERENCES SpotifyClone.subscriptions(subscriptions_id)
-) ENGINE = Innodb;
+  plan_id INT,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW(),
+  CHECK (age > 0),
+  FOREIGN KEY (plan_id)
+    REFERENCES plans(id)
+    ON DELETE SET NULL
+) engine = InnoDB;
 
-INSERT INTO SpotifyClone.users (artist_id, user_name, age, subscriptions_id)
-VALUES
-  (1, 'Thati', 23, 1),
-  (2, 'Cintia', 35, 2),
-  (3, 'Bill', 20, 3),
-  (4, 'Roger', 45, 1);
+CREATE TABLE artists(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW()
+) engine = InnoDB;
 
-
-CREATE TABLE SpotifyClone.albuns (
-  album_id INT PRIMARY KEY,
-  album VARCHAR(100),
-  artist_id INT,
-  FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artists(artist_id)
-) ENGINE = Innodb;
-
-INSERT INTO SpotifyClone.albuns (album_id, album, artist_id)
-VALUES
-  (1, 'Envious', 4),
-  (2, 'Exuberant', 4),
-  (3, 'Hallowed Steam', 3),
-  (4, 'Incandescent', 2),
-  (5, 'Temporary Culture', 1);
-
-CREATE TABLE SpotifyClone.songs (
-song_id	INT PRIMARY KEY,
-song VARCHAR(100),
-album_id INT,
-FOREIGN KEY (album_id) REFERENCES SpotifyClone.albuns(album_id)
-) ENGINE =  Innodb;
-
-INSERT INTO SpotifyClone.songs (song_id, song, album_id)
-VALUES
-  (1, "Thang Of Thunder", 5),
-  (2, "Words Of Her Life", 5),
-  (3, "Without My Streets", 5),
-  (4, "Fantasy For Me", 4),
-  (5, "Celebration Of More", 4),
-  (6, "Rock His Everything", 4),
-  (7, "Home Forever", 4),
-  (8, "Diamond Power", 4),
-  (9, "Honey, Let's Be Silly", 4),
-  (10, "Magic Circus", 3),
-  (11, "Honey, So Do I", 3),
-  (12, "Sweetie, Let's Go Wild", 3),
-  (13, "She Knows", 3),
-  (14, "Soul For Us", 1),
-  (15, "Reflections Of Magic", 1),
-  (16, "Dance With Her Own", 1),
-  (17, "Troubles Of My Inner Fire", 2),
-  (18, "Time Fireworks", 2);
-
-CREATE TABLE SpotifyClone.history_song (
-  artist_id INT NOT NULL,
-  song_id INT NOT NULL,
-  PRIMARY KEY (artist_id, song_id),
-  FOREIGN KEY (artist_id) REFERENCES SpotifyClone.users(artist_id),
-  FOREIGN KEY (song_id) REFERENCES SpotifyClone.songs(song_id)
-) ENGINE = Innodb;
-
-INSERT INTO SpotifyClone.history_song (artist_id, song_id)
-VALUES
-  (1, 8),
-  (1, 10),
-  (1, 14),
-  (1, 1),
-  (2, 7),
-  (2, 9),
-  (2, 15),
-  (2, 2),
-  (3, 10),
-  (3, 1),
-  (3, 17),
-  (4, 5),
-  (4, 16),
-  (4, 3);
-
-CREATE TABLE SpotifyClone.user_artist (
-  artist_id INT NOT NULL,
+CREATE TABLE follows(
+  PRIMARY KEY(user_id, artist_id),
   user_id INT NOT NULL,
-  PRIMARY KEY (artist_id, user_id),
-  FOREIGN KEY (artist_id) REFERENCES SpotifyClone.users(artist_id),
-  FOREIGN KEY (user_id) REFERENCES SpotifyClone.artists(user_id)
-) ENGINE = Innodb;
+  artist_id INT NOT NULL,
+  created_at DATETIME DEFAULT NOW(),
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (artist_id)
+    REFERENCES artists(id)
+    ON DELETE CASCADE
+) engine = InnoDB;
 
-INSERT INTO SpotifyClone.user_artist (artist_id, user_id)
+CREATE TABLE albums(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  artist_id INT NOT NULL,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW(),
+  FOREIGN KEY (artist_id)
+    REFERENCES artists(id)
+    ON DELETE CASCADE
+) engine = InnoDB;
+
+CREATE TABLE songs(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  duration INT NOT NULL,
+  artist_id INT NOT NULL,
+  album_id INT,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW(),
+  FOREIGN KEY (artist_id)
+    REFERENCES artists(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (album_id)
+    REFERENCES albums(id)
+    ON DELETE SET NULL
+) engine = InnoDB;
+
+CREATE TABLE song_plays(
+  PRIMARY KEY(user_id, song_id),
+  user_id INT NOT NULL,
+  song_id INT NOT NULL,
+  played_at DATETIME DEFAULT NOW(),
+  FOREIGN KEY (song_id)
+    REFERENCES songs(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+) engine = InnoDB;
+
+INSERT INTO plans (name, price)
 VALUES
-  (1, 4),
+  ('gratuito', '0'),
+  ('familiar', '7.99'),
+  ('universitário', '5.99');
+
+INSERT INTO users (name, email, age, plan_id)
+VALUES
+  ('Thati', 'thati@gmail.com', 23, 1),
+  ('Cintia', 'cintia@gmail.com', 35, 2),
+  ('Bill', 'bill@gmail.com', 20, 3),
+  ('Roger', 'roger@gmail.com', 45, 1);
+
+INSERT INTO artists (name)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
+INSERT INTO follows (user_id, artist_id)
+VALUES
   (1, 1),
-  (1, 2),
-  (2, 4),
+  (1, 3),
+  (1, 4),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
+
+INSERT INTO albums (name, artist_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+
+INSERT INTO songs (name, duration, artist_id, album_id)
+VALUES
+  ('Soul For Us', 180, 1, 1),
+  ('Reflections Of Magic', 240, 1, 1),
+  ('Dance With Her Own', 240, 1, 1),
+  ('Troubles Of My Inner Fire', 180, 1, 2),
+  ('Time Fireworks', 175, 1, 2),
+  ('Magic Circus', 180, 2, 3),
+  ('Honey, So Do I', 186, 2, 3),
+  ("Sweetie, Let\'s Go Wild", 180, 2, 3),
+  ('She Knows', 197, 2, 3),
+  ('Fantasy For Me', 197, 3, 4),
+  ('Celebration Of More', 197, 3, 4),
+  ('Rock His Everything', 197, 3, 4),
+  ('Home Forever', 197, 3, 4),
+  ('Diamond Power', 197, 3, 4),
+  ("Honey, Let\'s Be Silly", 197, 3, 4),
+  ('Thang Of Thunder', 197, 4, 5),
+  ('Words Of Her Life', 197, 4, 5),
+  ('Without My Streets', 197, 4, 5);
+
+INSERT INTO song_plays (user_id, song_id)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
   (2, 2),
-  (3, 3),
+  (2, 15),
   (3, 4),
-  (4, 1);
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
