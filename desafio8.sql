ca4f6f31832fc393ab39ad17d1878fa6d868b6fc
Anderson R <misterregisslv@gmail.com>
2021-01-18 17:07:03 -0300

    desafio 8 refeito 2x

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,7 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete AFTER DELETE ON SpotifyClone.usuario FOR EACH ROW
 BEGIN
-SET foreign_key_checks = 0;
-DELETE FROM SpotifyClone.seguidor_artista WHERE seguidor_artista.usuario_id = OLD.usuario_id;
 DELETE FROM SpotifyClone.historico_reproducao WHERE historico_reproducao.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguidor_artista WHERE seguidor_artista.usuario_id = OLD.usuario_id;
 END $$;
 DELIMITER ;
