5c29e6276f9073f673c658f0a6ac5c231fe2dfe9
Kesley Lahoz <namielahoz@gmail.com>
2021-01-18 10:29:06 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quantity INT;
+    SELECT COUNT(*) AS quantidade_musicas_no_historico
+    FROM historico_reproducao
+    WHERE usuario_id = id
+    INTO quantity;
+    RETURN quantity;
+END $$
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
