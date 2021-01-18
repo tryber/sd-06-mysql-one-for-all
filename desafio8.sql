13d858dd6b93ee7f403cb8947a0a6bad869b7f2b
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-18 16:15:00 -0300

    req8

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER `trigger_usuario_delete`
 BEFORE DELETE ON userstable
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.usuario_id;
+DELETE FROM reproductiontable WHERE user_id = OLD.user_id;
+DELETE FROM followerstable WHERE user_id = OLD.user_id;
 END$
 
 DELIMITER ;
