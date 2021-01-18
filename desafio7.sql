3eddce6639109b5b06641bca1295a3a03cf8e7d0
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-18 14:35:43 -0300

    req7

--- desafio7.sql
+++ desafio7.sql
@@ -9,5 +9,5 @@ INNER JOIN userstable
 ON followerstable.user_id = userstable.user_id)
 INNER JOIN albunstable
 ON followerstable.artist_id = albunstable.artist_id)
-group by `album` 
+group by `artista`, `album` 
 order by `seguidores` desc, `artista` asc;
