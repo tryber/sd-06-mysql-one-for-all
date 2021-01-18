bfdfc049760862af74a0b126490cc82a04fd2014
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-18 14:22:06 -0300

    req6 e req7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,3 @@
-CREATE VIEW `perfil_artistas` AS
-SELECT artiststable.artist_name AS `artista`,
 albunstable.album_name AS `album`,
 count(followerstable.artist_id) AS `seguidores`
 FROM (((followerstable
@@ -9,5 +7,5 @@ INNER JOIN userstable
 ON followerstable.user_id = userstable.user_id)
 INNER JOIN albunstable
 ON followerstable.artist_id = albunstable.artist_id)
-group by albunstable.album_name 
+group by `album` 
 order by `seguidores` desc, `artista` asc;
