96071482634e24102978cf29c969fe2dc137a66b
Paulo Frazão <paulo4187@gmail.com>
2021-01-18 17:43:19 -0300

    correction insert tables

--- desafio1.sql
+++ desafio1.sql
@@ -53,64 +53,79 @@ PRIMARY KEY (usuario_id, cancao_id)
 )engine = InnoDB;
 
 
-INSERT INTO SpotifyClone.planos (plano_nome,valor) VALUES ('Gratuito',0);
-INSERT INTO SpotifyClone.planos (plano_nome,valor) VALUES ('Familiar',7.99);
-INSERT INTO SpotifyClone.planos (plano_nome,valor) VALUES ('Universitário',5.99);
+INSERT INTO SpotifyClone.planos (plano_nome,valor)
+VALUES
+('Gratuito',0),
+('Familiar',7.99),
+('Universitário',5.99);
 
-INSERT INTO SpotifyClone.usuarios (usuario_nome,idade,user_plano_id) VALUES ('Thati',23,1);
-INSERT INTO SpotifyClone.usuarios (usuario_nome,idade,user_plano_id) VALUES ('Cintia',35,2);
-INSERT INTO SpotifyClone.usuarios (usuario_nome,idade,user_plano_id) VALUES ('Bill',20,3);
-INSERT INTO SpotifyClone.usuarios (usuario_nome,idade,user_plano_id) VALUES ('Roger',45,1);
+INSERT INTO SpotifyClone.usuarios (usuario_nome,idade,user_plano_id)
+VALUES 
+('Thati',23,1),
+('Cintia',35,2),
+('Bill',20,3),
+('Roger',45,1);
 
-INSERT INTO SpotifyClone.artista (artista_nome) VALUES ('Walter Phoenix');
-INSERT INTO SpotifyClone.artista (artista_nome) VALUES ('Peter Strong');
-INSERT INTO SpotifyClone.artista (artista_nome) VALUES ('Lance Day');
-INSERT INTO SpotifyClone.artista (artista_nome) VALUES ('Freedie Shannon');
+INSERT INTO SpotifyClone.artista (artista_nome) 
+VALUES 
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (1,1);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (1,4);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (1,3);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (2,1);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (2,3);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (3,2);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (3,1);
-INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) VALUES (4,4);
+INSERT INTO SpotifyClone.usuario_segue (usuario_id, artista_id) 
+VALUES 
+(1,1),
+(1,4),
+(1,3),
+(2,1),
+(2,3),
+(3,2),
+(3,1),
+(4,4);
 
-INSERT INTO SpotifyClone.albuns (album_nome, artista_id) VALUES ('Envious',1);
-INSERT INTO SpotifyClone.albuns (album_nome, artista_id) VALUES ('Exuberant',1);
-INSERT INTO SpotifyClone.albuns (album_nome, artista_id) VALUES ('Hallowed Steam',2);
-INSERT INTO SpotifyClone.albuns (album_nome, artista_id) VALUES ('Incandescent',3);
-INSERT INTO SpotifyClone.albuns (album_nome, artista_id) VALUES ('Temporary Culture',4);
+INSERT INTO SpotifyClone.albuns (album_nome, artista_id) 
+VALUES 
+('Envious',1),
+('Exuberant',1),
+('Hallowed Steam',2),
+('Incandescent',3),
+('Temporary Culture',4);
 
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Soul For Us',1);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Reflections Of Magic',1);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Dance With Her Own',1);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Troubles Of My Inner Fire',2);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Time Fireworks',2);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Magic Circus',3);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Honey, So Do I',3);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ("Sweetie, Let's Go Wild",3);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('She Knows',3);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Fantasy For Me',4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Celebration Of More',4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Rock His Everything',4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Home Forever',4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Diamond Power',4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ("Honey, Let's Be Silly",4);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Thang Of Thunder',5);
-INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) VALUES ('Words Of Her Life',5);
+INSERT INTO SpotifyClone.cancoes (cancao_nome, cancoes_album_id) 
+VALUES 
+('Soul For Us',1),
+('Reflections Of Magic',1),
+('Dance With Her Own',1),
+('Troubles Of My Inner Fire',2),
+('Time Fireworks',2),
+('Magic Circus',3),
+('Honey, So Do I',3),
+("Sweetie, Let's Go Wild",3),
+('She Knows',3),
+('Fantasy For Me',4),
+('Celebration Of More',4),
+('Rock His Everything',4),
+('Home Forever',4),
+('Diamond Power',4),
+("Honey, Let's Be Silly",4),
+('Thang Of Thunder',5),
+('Words Of Her Life',5),
+('Without My Streets',5);
 
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (1,1);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (1,6);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (1,14);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (1,16);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (2,13);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (2,17);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (2,2);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (2,15);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (3,4);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (3,16);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (3,6);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (4,3);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (4,18);
-INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) VALUES (4,11);
+INSERT INTO SpotifyClone.historico_reproducao (usuario_id, cancao_id) 
+VALUES 
+(1,1),
+(1,6),
+(1,14),
+(1,16),
+(2,13),
+(2,17),
+(2,2),
+(2,15),
+(3,4),
+(3,16),
+(3,6),
+(4,3),
+(4,18),
+(4,11);
