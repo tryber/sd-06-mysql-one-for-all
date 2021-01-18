cea251bae402eafee57e4ea814654b500fd3843d
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-18 17:41:56 -0300

    req9

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,6 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `albuns_do_artista`(IN `artista` VARCHAR(100))
+
+CREATE PROCEDURE `albuns_do_artista`(IN `artista` VARCHAR(100))
 BEGIN
 SELECT artiststable.artist_name AS `artista`,
 albunstable.album_name AS `album`
@@ -8,4 +9,5 @@ INNER JOIN albunstable
 ON artiststable.artist_id = albunstable.artist_id)
 order by `artista` desc limit 2;
 END$$
+
 DELIMITER ;
