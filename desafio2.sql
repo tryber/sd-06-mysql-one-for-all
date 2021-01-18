7d16bd556c26d4a9183c53c32b757c739b22bb1d
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-01-18 12:47:33 -0300

    Desafio 2 feito

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT COUNT(cancao_id) AS cancoes,
+(SELECT COUNT(artista_id)
+FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(album_id)
+FROM SpotifyClone.albuns) AS albuns
+FROM SpotifyClone.cancoes;
