73977371e6c76ff5ac7ff1750ed3851b12300acb
FlavioSugano <flavio.sugano@gmail.com>
2021-01-18 19:42:31 -0300

    Desafio 3: correção no ORDER BY

--- desafio3.sql
+++ desafio3.sql
@@ -7,4 +7,7 @@ CREATE VIEW historico_reproducao_usuarios AS
         SpotifyClone.users AS u ON h.user_id = u.user_id
             INNER JOIN
         SpotifyClone.songs AS s ON h.song_id = s.song_id
-    ORDER BY usuario;
+    ORDER BY usuario , nome;
+
+select * from historico_reproducao_usuarios;
+drop view historico_reproducao_usuarios;
