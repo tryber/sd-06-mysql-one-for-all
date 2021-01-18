142ac626e61732aedc1ea1fead01f68aabe4d820
kissyla <kissylamottapereira@gmail.com>
2021-01-18 18:21:08 -0300

    desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS 
-SELECT C.CA AS 'cancoes',
-A.AR  AS 'artistas',
-ALB.AL AS 'albuns'
-FROM (SELECT COUNT(*) AS CA FROM SpotifyClone.cancoes) AS C,
-(SELECT COUNT(*) AS AR FROM SpotifyClone.artistas) AS A,
-(SELECT COUNT(*) AS AL FROM SpotifyClone.album) AS ALB
+SELECT COUNT(DISTINCT CA.cancao_id) AS 'cancoes',
+COUNT(DISTINCT AR.artista_id)  AS 'artistas',
+COUNT(DISTINCT AL.album_id) AS 'albuns'
+FROM SpotifyClone.cancoes AS CA,
+     SpotifyClone.artistas AS AR,
+	 SpotifyClone.album AS AL
