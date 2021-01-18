454061643eb2d187408b7b2e240db9ebd88fc8fb
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-18 18:26:47 -0300

    req8 testing

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,9 @@
-USE SpotifyClone;
-
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON users
-FOR EACH ROW
+    BEFORE DELETE ON SpotifyClone.users
+    FOR EACH ROW
 BEGIN
-DELETE FROM songs_played WHERE user_id = OLD.user_id;
-DELETE FROM artists_following WHERE user_id = OLD.user_id;
+    DELETE FROM SpotifyClone.songs_played WHERE songs_played.user_id = OLD.user_id;
+    DELETE FROM SpotifyClone.artists_following WHERE artists_following.user_id = OLD.user_id;
 END; $$
 DELIMITER ;
