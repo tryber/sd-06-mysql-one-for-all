b6d1c32c058883a20e13cc6f953c58b73d33d25b
VinicinBraga <vinicius@bragamatta.com.br>
2021-01-18 21:11:48 -0300

    Desafios 6 e 7 - new try

--- desafio6.sql
+++ desafio6.sql
@@ -5,5 +5,4 @@ ROUND(MAX(fat.plano_valor), 2) AS faturamento_maximo,
 ROUND(AVG(fat.plano_valor), 2) AS faturamento_medio,
 ROUND(SUM(fat.plano_valor), 2) AS faturamento_total
 FROM planos AS fat
-INNER JOIN user AS u
-ON u.plano_id = fat.plano_id;
+INNER JOIN user AS u ON u.plano_id = fat.plano_id;
