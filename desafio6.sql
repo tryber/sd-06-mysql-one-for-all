e9bf884b433802ddf5a31903881746752182577f
Paulo Frazão <paulo4187@gmail.com>
2021-01-19 17:07:17 -0300

    convert int as char challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
-CREATE VIEW faturamento_atual AS SELECT
+CREATE VIEW faturamento_atual AS; SELECT
 CONCAT(MIN(P.valor),'.','00') faturamento_minimo, CONCAT(ROUND(MAX(P.valor),2)) faturamento_maximo,
 CONCAT(ROUND((SELECT SUM(P.valor) FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
 ON U.user_plano_id = P.plano_id)/(SELECT COUNT(user_plano_id) FROM SpotifyClone.usuarios),2))
-AS faturamento_medio,  CONCAT(SUM(P.valor)) faturamento_total 
+AS faturamento_medio,  CONVERT(SUM(P.valor), char(5)) faturamento_total 
 FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
 ON U.user_plano_id = P.plano_id;
