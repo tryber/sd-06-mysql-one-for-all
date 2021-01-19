d4c8553eac0cdf7fe6e8501bb93b8d83fbf7ca2c
Pedro Eduardo <pedroedu77@gmail.com>
2021-01-19 15:36:54 -0300

    Req 8 reforumlating

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.song_played WHERE song_played.user_id = OLD.user_id;
-DELETE FROM SpotifyClone.artists_following WHERE artists_following.user_id = OLD.user_id;
+	DELETE FROM SpotifyClone.song_played WHERE user_id = OLD.user_id;
+	DELETE FROM SpotifyClone.artists_following WHERE user_id = OLD.user_id;
 END; $$
 DELIMITER ;
