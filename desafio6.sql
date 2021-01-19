88a5a5a0d60a5a8ec369da76dfe95a41d59cb669
Paulo Frazão <paulo4187@gmail.com>
2021-01-19 17:15:42 -0300

    convert int CREATE VIEW faturamento_atual AS SELECT challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 CREATE VIEW faturamento_atual AS SELECT
-CONCAT(MIN(P.valor),'.','00') faturamento_minimo, CONCAT(ROUND(MAX(P.valor),2)) faturamento_maximo,
-CONCAT(ROUND((SELECT SUM(P.valor) FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
-ON U.user_plano_id = P.plano_id)/(SELECT COUNT(user_plano_id) FROM SpotifyClone.usuarios),2))
-AS faturamento_medio,  CONVERT(SUM(P.valor), char(5)) faturamento_total 
+CONCAT(MIN(P.valor),'.','00') faturamento_minimo, CONVERT(ROUND(MAX(P.valor),2), CHARACTER) faturamento_maximo,
+CONVERT(ROUND((SELECT SUM(P.valor) FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
+ON U.user_plano_id = P.plano_id)/(SELECT COUNT(user_plano_id) FROM SpotifyClone.usuarios),2), CHARACTER)
+AS faturamento_medio,  CONVERT(SUM(P.valor), CHARACTER) faturamento_total 
 FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
 ON U.user_plano_id = P.plano_id;
