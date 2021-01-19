38168f530e31e67b7eb6e453b7ca22efa661140b
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-01-19 00:17:18 -0300

    Fixed code style from challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,21 +1,21 @@
-CREATE VIEW perfil_artistas AS
-  SELECT
-    ar.artist_name AS `artista`,
-    al.album_name AS `album`,
-    artist_ranking.quantity AS `seguidores`
-  FROM
-    artists AS ar
-  INNER JOIN albums AS al ON ar.id = al.artist_id
+CREATE VIEW perfil_artistas AS 
+SELECT 
+  ar.artist_name AS `artista`, 
+  al.album_name AS `album`, 
+  artist_ranking.quantity AS `seguidores` 
+FROM 
+  artists AS ar 
+  INNER JOIN albums AS al ON ar.id = al.artist_id 
   INNER JOIN (
-    SELECT
-      artist_id,
-      COUNT(artist_id) AS quantity
-	  FROM
-      follow
-	  GROUP BY
-      artist_id) AS artist_ranking
-        ON ar.id = artist_ranking.artist_id
-  ORDER BY
-    `seguidores` DESC,
-    `artista` ASC,
-    `album` ASC;
+    SELECT 
+      artist_id, 
+      COUNT(artist_id) AS quantity 
+    FROM 
+      follow 
+    GROUP BY 
+      artist_id
+  ) AS artist_ranking ON ar.id = artist_ranking.artist_id 
+ORDER BY 
+  `seguidores` DESC, 
+  `artista` ASC, 
+  `album` ASC;
