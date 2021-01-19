c0ccd772352da32fe4e8366be334c5a00ee505a0
Paulo Frazão <paulo4187@gmail.com>
2021-01-19 16:35:55 -0300

    change challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,8 @@
 CREATE VIEW faturamento_atual AS SELECT
-ROUND(MIN(P.valor),2) faturamento_minimo, ROUND(MAX(P.valor),2) faturamento_maximo,
-ROUND((SELECT SUM(P.valor) FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
-ON U.user_plano_id = P.plano_id)/(SELECT COUNT(user_plano_id) FROM SpotifyClone.usuarios),2)
-AS faturamento_medio,  SUM(P.valor) faturamento_total 
-FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
+ROUND(MIN(P.valor), 2) AS faturamento_minimo,
+ROUND(MAX(P.valor), 2) AS faturamento_maximo,
+ROUND(AVG(P.valor), 2) AS faturamento_medio,
+ROUND(SUM(P.valor), 2) AS faturamento_total
+FROM SpotifyClone.usuarios AS U
+LEFT JOIN planos AS P
 ON U.user_plano_id = P.plano_id;
