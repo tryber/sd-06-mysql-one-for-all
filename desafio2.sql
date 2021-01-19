0a15cc3c899d72de6fd53a8970acf99bbbfccf9c
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-19 19:12:13 -0300

    FIX: Desafio 02.1

--- desafio2.sql
+++ desafio2.sql
@@ -4,10 +4,8 @@
 -- A terceira coluna deve exibir a quantidade de álbuns e deverá ter o alias "albuns".
 -- Sua VIEW deve retornar a seguinte informação ao ser consultada:
 -- cancoes 18 - artistas 4 - albuns 5
-USE SpotifyClone;
-
-DROP DATABASE IF EXISTS estatisticas_musicais;
-
+-- USE SpotifyClone;
+-- DROP DATABASE IF EXISTS estatisticas_musicais;
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(DISTINCT SpotifyClone.cancoes.cancoes_nome) AS cancoes,
 COUNT(DISTINCT SpotifyClone.artista.artista_nome) AS artistas,
