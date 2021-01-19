23972520781219ebc4dcfd1f6ef1584a3eda60c6
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-01-19 11:56:16 -0300

    Refatoraçao

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,13 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuario_filtro VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(usuario_filtro TINYINT)
 RETURNS TINYINT READS SQL DATA
 
 BEGIN
 DECLARE quantidade_musicas TINYINT;
 SELECT COUNT(hi.musica_id)
 FROM SpotifyClone.historico AS hi 
-INNER JOIN SpotifyClone.usuarios AS us
-ON hi.usuario_id = us.usuario_id
-WHERE us.usuario = usuario_filtro INTO quantidade_musicas;
+WHERE us.usuario_id = usuario_filtro INTO quantidade_musicas;
   RETURN quantidade_musicas;
 END $$
 
