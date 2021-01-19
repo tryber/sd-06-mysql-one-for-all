370a2216b423ac789f9ab29ce948f4e5411a92ec
LughWalle <civalfran1@gmail.com>
2021-01-19 03:16:57 -0300

    desafio 4 fix 2

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,11 @@
-CREATE VIEW top_3_artistas AS
-SELECT ar.artist AS artista,
-COUNT(uf.user_id) AS seguidores
-FROM user_following AS uf
-INNER JOIN artists AS ar
-ON ar.artist_id = uf.artist_id
-GROUP BY 1
-ORDER BY 2 DESC,
-1 LIMIT 3;
+	CREATE VIEW top_3_artistas AS
+	SELECT ar.artist AS artista,
+	COUNT(uf.user_id) AS seguidores
+	FROM user_following AS uf
+	INNER JOIN artists AS ar
+	ON ar.artist_id = uf.artist_id
+	GROUP BY 1
+	ORDER BY 2 DESC,
+	1 LIMIT 3;
+
  
