b6895be5a5b917a6ef6497b1b68ffefbf284026c
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-01-19 20:51:49 -0300

    Excluindo teste

--- desafio2.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW estatisticas_musicais AS
-SELECT COUNT(DISTINCT SpotifyClone.cd.musica_id) AS cancoes,
-COUNT(DISTINCT SpotifyClone.interpretes.artista_id) AS interpretes,
-COUNT(DISTINCT SpotifyClone.albums.album_id) AS albums
-FROM SpotifyClone.cd, SpotifyClone.interpretes, SpotifyClone.albums;
