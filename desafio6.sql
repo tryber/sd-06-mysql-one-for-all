7cbfb80dcf8ae84a49acecc469cfaa557ba063f1
Paulo Frazão <paulo4187@gmail.com>
2021-01-19 17:10:36 -0300

    convert int as char, remove semicolon  challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-CREATE VIEW faturamento_atual AS; SELECT
+CREATE VIEW faturamento_atual AS SELECT
 CONCAT(MIN(P.valor),'.','00') faturamento_minimo, CONCAT(ROUND(MAX(P.valor),2)) faturamento_maximo,
 CONCAT(ROUND((SELECT SUM(P.valor) FROM SpotifyClone.planos P INNER JOIN SpotifyClone.usuarios U
 ON U.user_plano_id = P.plano_id)/(SELECT COUNT(user_plano_id) FROM SpotifyClone.usuarios),2))
