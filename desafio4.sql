8d57c5ff14b3000cb0c3ed09e6e01ca62daa8982
FlavioSugano <flavio.sugano@gmail.com>
2021-01-18 22:00:21 -0300

    Desafio 4: agora c/ a query de fato

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        a.artist_name AS 'artista',
+        COUNT(user_id) AS 'seguidores'
+    FROM
+        SpotifyClone.following AS f
+            INNER JOIN
+        SpotifyClone.artists AS a ON f.artist_id = a.artist_id
+    GROUP BY artista
+    ORDER BY seguidores DESC , artista ASC
+    LIMIT 3;
