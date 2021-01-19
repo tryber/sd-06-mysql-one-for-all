348a01a7bb98056f548c61043fc0e70745d3fa81
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-01-19 19:44:52 -0300

    Iniciando o projeto, desafio1.

---
+++ desafio1.sql
@@ -0,0 +1,131 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE interpretes(
+artista_id INT PRIMARY KEY AUTO_INCREMENT,
+interprete VARCHAR(100) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE pacotes(
+plano_id INT PRIMARY KEY AUTO_INCREMENT,
+pacote VARCHAR(50) NOT NULL,
+valor_plano DECIMAL(4, 2)
+) engine = InnoDB;
+
+CREATE TABLE assinantes(
+usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+assinante VARCHAR(30) NOT NULL,
+idade INT NOT NULL,
+plano_id int,
+FOREIGN KEY(plano_id) REFERENCES pacotes(plano_id)
+) engine = InnoDB;
+
+CREATE TABLE albums(
+album_id INT PRIMARY KEY AUTO_INCREMENT,
+album VARCHAR(255) NOT NULL,
+artista_id INT,
+FOREIGN KEY(artista_id) REFERENCES interpretes(artista_id)
+) engine = InnoDB;
+
+CREATE TABLE cd(
+musica_id INT PRIMARY KEY AUTO_INCREMENT,
+musica VARCHAR(255) NOT NULL,
+album_id INT,
+FOREIGN KEY(album_id) REFERENCES albums(album_id)
+) engine = InnoDB;
+
+CREATE TABLE historico(
+usuario_id INT,
+musica_id INT,
+PRIMARY KEY(usuario_id, musica_id),
+FOREIGN KEY(usuario_id) REFERENCES assinantes(usuario_id),
+FOREIGN KEY(musica_id) REFERENCES cd(musica_id)
+) engine = InnoDB;
+
+CREATE TABLE seguidores(
+usuario_id INT,
+artista_id INT,
+PRIMARY KEY(usuario_id, artista_id),
+FOREIGN KEY(usuario_id) REFERENCES assinantes(usuario_id),
+FOREIGN KEY(artista_id) REFERENCES interprete(artista_id)
+) engine = InnoDB;
+
+INSERT INTO interpretes (interprete)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO pacotes (pacote, valor_plano)
+VALUES
+('gratuito', 0.00),
+('familiar', 7.99),
+('universitário', 5.99);
+
+INSERT INTO assinantes (assinante, idade, plano_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO albums (album, artista_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO cd (musica, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO historico (usuario_id, musica_id)
+VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
+
+INSERT INTO seguidores (usuario_id, artista_id)
+VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
