340aaf332158eb77a56eb341f3718a7989c8b31b
FlavioSugano <flavio.sugano@gmail.com>
2021-01-18 22:59:18 -0300

    Desafio 6: correção nas casas decimais

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 CREATE VIEW faturamento_atual AS
     SELECT 
-        MIN(p.plan_value) AS faturamento_minimo,
-        MAX(p.plan_value) AS faturamento_maximo,
+        ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
+        ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
         ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
         ROUND(SUM(p.plan_value), 2) AS faturamento_total
     FROM
