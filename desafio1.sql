7d987a6afa6957f34967f93f8e96c60ed9ed933b
LughWalle <civalfran1@gmail.com>
2021-01-18 21:05:09 -0300

    desafio1 fix CC

--- desafio1.sql
+++ desafio1.sql
@@ -5,52 +5,52 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plans(
-	plan_id INT PRIMARY KEY AUTO_INCREMENT,
-    plan_type VARCHAR (14) NOT NULL,
-    value_plan numeric (4,2) NOT NULL
+plan_id INT PRIMARY KEY AUTO_INCREMENT,
+plan_type VARCHAR (14) NOT NULL,
+value_plan numeric (4,2) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE artists(
-	artist_id INT PRIMARY KEY AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
+artist VARCHAR(50) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE users(
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR (30) NOT NULL,
-    year_old INT NOT NULL,
-    plan_id INT NOT NULL,
-    FOREIGN KEY (plan_id) REFERENCES plans (plan_id) 
+user_id INT PRIMARY KEY AUTO_INCREMENT,
+user_name VARCHAR (30) NOT NULL,
+year_old INT NOT NULL,
+plan_id INT NOT NULL,
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id) 
 ) ENGINE = InnoDB;
 
 CREATE TABLE albums(
-	album_id INT PRIMARY KEY AUTO_INCREMENT,
-    album VARCHAR (30) NOT NULL,
-    artist_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+album_id INT PRIMARY KEY AUTO_INCREMENT,
+album VARCHAR (30) NOT NULL,
+artist_id INT NOT NULL,
+FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE songs(
-	song_id INT PRIMARY KEY AUTO_INCREMENT,
-    song VARCHAR (50) NOT NULL,
-    album_id INT NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES albums (album_id)
+song_id INT PRIMARY KEY AUTO_INCREMENT,
+song VARCHAR (50) NOT NULL,
+album_id INT NOT NULL,
+FOREIGN KEY (album_id) REFERENCES albums (album_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE reproduction_history(
-	user_id INT NOT NULL,
-    song_id INT NOT NULL,
-	PRIMARY KEY (user_id, song_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id),
-	FOREIGN KEY (song_id) REFERENCES songs (song_id)
+user_id INT NOT NULL,
+song_id INT NOT NULL,
+PRIMARY KEY (user_id, song_id),
+FOREIGN KEY (user_id) REFERENCES users (user_id),
+FOREIGN KEY (song_id) REFERENCES songs (song_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE user_following(
-	user_id INT NOT NULL,
-    artist_id INT NOT NULL,
-    PRIMARY KEY (user_id, artist_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id),
-	FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+user_id INT NOT NULL,
+artist_id INT NOT NULL,
+PRIMARY KEY (user_id, artist_id),
+FOREIGN KEY (user_id) REFERENCES users (user_id),
+FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) ENGINE = InnoDB;
 
 INSERT INTO plans(plan_type, value_plan)
