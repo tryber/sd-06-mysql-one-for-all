8cb59da6c2659fc8b288c0516eb2a5bb658dcfc1
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 22:32:43 -0300

    Refatoring

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
-CREATE VIEW estatisticas_musicais AS
-SELECT 
+CREATE VIEW estatisticas_musicais AS SELECT 
 COUNT(DISTINCT m.music_name) AS cancoes,
 COUNT(DISTINCT a.artist_name) AS artistas,
 COUNT(DISTINCT al.album_name) AS albums
-FROM 
-SpotifyClone.musics AS m,
-SpotifyClone.artists AS a,
-SpotifyClone.albums AS al;
+FROM SpotifyClone.musics AS m
+JOIN SpotifyClone.artists AS a
+JOIN SpotifyClone.albums AS al;
