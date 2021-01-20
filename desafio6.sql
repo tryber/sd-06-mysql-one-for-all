d95e4d74095b75d7f298d38347b9b1a0847a4160
ilanribeiro <ilanribeiro@gmail.com>
2021-01-20 05:49:57 -0300

    change: correcting requirement 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(valor_plano) AS 'faturamento_minimo',
-MAX(valor_plano) AS 'faturamento_maximo',
-ROUND(AVG(valor_plano), 2) AS 'faturamento_medio',
-SUM(valor_plano) AS 'faturamento_total'
-FROM SpotifyClone.plano;
+MIN(plano.valor_plano) AS 'faturamento_minimo',
+MAX(plano.valor_plano) AS 'faturamento_maximo',
+ROUND(AVG(plano.valor_plano), 2) AS 'faturamento_medio',
+SUM(plano.valor_plano) AS 'faturamento_total'
+FROM SpotifyClone.plano AS plano
+INNER JOIN SpotifyClone.usuario AS usuario
+ON plano.plano_id = usuario.plano_id;
