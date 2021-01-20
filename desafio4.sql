3a3ea00ede3788e2216eecd13352005438415c3a
PAULO LINS <paulolins100@gmail.com>
2021-01-20 01:20:23 -0300

    desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,8 @@
-CREATE VIEW top_3_artistas AS
-SELECT
-a.artista_name AS `artista`,
-COUNT(u.usuario_id) AS `seguidores`
+CREATE VIEW top_3_artistas AS SELECT
+a.artista_name AS artista,
+COUNT(u.usuario_id) AS seguidores
 FROM SpotifyClone.artista AS a
-INNER JOIN SpotifyClone.artista_usuario AS u
+INNER JOIN SpotifyClone.artista_usuario u
 ON a.artista_id = u.artista_id
-GROUP BY `artista`
-ORDER BY `seguidores` DESC LIMIT 3;
+GROUP BY artista
+ORDER BY seguidores DESC, artista LIMIT 3;
