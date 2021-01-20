ec9a26369323d6240ba9a4425ce2885b4fd05e62
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 23:24:05 -0300

    Refatoring

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(DISTINCT m.music_name) AS cancoes,
 COUNT(DISTINCT a.artist_name) AS artistas,
-COUNT(DISTINCT al.album_name) AS albums
+COUNT(DISTINCT al.album_name) AS albuns
 FROM musics AS m
 JOIN artists AS a
 JOIN albums AS al;
