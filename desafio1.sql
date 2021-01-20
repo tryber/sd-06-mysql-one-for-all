40ee9ae4ef1d9ed0015c6b6d67c97864e9ae51fd
ilanribeiro <ilanribeiro@gmail.com>
2021-01-20 04:09:00 -0300

    test

--- desafio1.sql
+++ desafio1.sql
@@ -9,7 +9,8 @@ CREATE TABLE `plano` (
 `plano_id` INT PRIMARY KEY AUTO_INCREMENT,
 `nome` VARCHAR(50) NOT NULL,
 `valor_plano` DECIMAL(4,2) NOT NULL
-) engine = InnoDb;
+)
+ENGINE = InnoDb;
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
@@ -18,13 +19,15 @@ CREATE TABLE `usuario` (
 `idade` INT NOT NULL,
 `plano_id` INT NOT NULL,
 FOREIGN KEY (`plano_id`) REFERENCES `plano`(`plano_id`)
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `artista`;
 CREATE TABLE `artista` (
 `artista_id` INT PRIMARY KEY AUTO_INCREMENT,
 `nome` VARCHAR(50) NOT NULL
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `album`;
 CREATE TABLE `album` (
@@ -32,7 +35,8 @@ CREATE TABLE `album` (
 `nome` VARCHAR(50) NOT NULL,
 `artista_id` INT NOT NULL,
 FOREIGN KEY (`artista_id`) REFERENCES `artista`(`artista_id`)
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `segue_artistas`;
 CREATE TABLE `segue_artistas` (
@@ -41,7 +45,8 @@ CREATE TABLE `segue_artistas` (
 PRIMARY KEY (`usuario_id`, `artista_id`),
 FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`usuario_id`),
 FOREIGN KEY (`artista_id`) REFERENCES `artista`(`artista_id`)
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `musica`;
 CREATE TABLE `musica` (
@@ -49,7 +54,8 @@ CREATE TABLE `musica` (
 `nome` VARCHAR(100) NOT NULL,
 `album_id` INT NOT NULL,
 FOREIGN KEY (`album_id`) REFERENCES `album`(`album_id`)
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `historico_reproducao`;
 CREATE TABLE `historico_reproducao` (
@@ -58,81 +64,75 @@ CREATE TABLE `historico_reproducao` (
 PRIMARY KEY (`musica_id`, `usuario_id`),
 FOREIGN KEY (`musica_id`) REFERENCES `musica`(`musica_id`),
 FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`usuario_id`)
-) engine = InnoDB;
+)
+ENGINE = InnoDB;
 
-INSERT INTO `plano` (`plano_id`, `nome`, `valor_plano`)
-VALUES
-(1,'individual',16.90),
-(2,'Duo',21.90),
-(3,'Família',26.90);
+INSERT INTO `plano` (`plano_id`, `nome`, `valor_plano`) VALUES
+(1,'gratuito',0),
+(2,'familiar',7.99),
+(3,'universitário',5.99);
 
-INSERT INTO `usuario` (`usuario_id`, `nome`, `idade`, `plano_id`)
-VALUES
-(1,'João',23,1),
-(2,'Marcos',35,2),
-(3,'Patricia',20,3),
-(4,'Alana',45,1);
+INSERT INTO `usuario` (`usuario_id`, `nome`, `idade`, `plano_id`) VALUES
+(1,'Thati',23,1),
+(2,'Cintia',35,2),
+(3,'Bill',20,3),
+(4,'Roger',45,1);
 
-INSERT INTO `artista` (`artista_id`, `nome`)
-VALUES
-(1,'Dire Straits'),
-(2,'Metallica'),
-(3,'Queen'),
-(4,'Beatles');
+INSERT INTO `artista` (`artista_id`, `nome`) VALUES
+(1,'Walter Phoenix'),
+(2,'Peter Strong'),
+(3,'Lance Day'),
+(4,'Freedie Shannon');
 
-INSERT INTO `album` (`album_id`, `nome`, `artista_id`)
-VALUES
-(1,'Metallica',2),
-(2,'Beatles Best of',4),
-(3,'A Night at the Opera',3),
-(4,'Brothers in Arms',1),
-(5,'Alchemy',1);
+INSERT INTO `album` (`album_id`, `nome`, `artista_id`) VALUES
+(1,'Envious',1),
+(2,'Exuberant',1),
+(3,'Hallowed Steam',2),
+(4,'Incandescent',3),
+(5,'Temporary Culture',4);
 
-INSERT INTO `segue_artistas` (`usuario_id`, `artista_id`)
-VALUES
+INSERT INTO `segue_artistas` (`usuario_id`, `artista_id`) VALUES
 (1,1),
-(1,2),
-(2,5),
-(2,7),
-(3,3),
-(3,6),
-(4,4),
-(4,1);
-
-INSERT INTO `musica` (`musica_id`, `nome`, `album_id`)
-VALUES
-(1,'Be Quick or Be Dead',1),
-(2,'From Here to Eternity',1),
-(3,'So Far Away',1),
-(4,'Money For Nothing',2),
-(5,'Walk of Life',2),
-(6,'Lazing On a Sunday Afternoon',3),
-(7,'Death on Two Legs',3),
-(8,"Banana Pancakes",3),
-(9,'In Between Dreams',3),
-(10,'Is This Love',4),
-(11,'Three Little Birds',4),
-(12,'Enter Sadman',4),
-(13,'Nothing Else Matters',4),
-(14,'Help',4),
-(15,'The Night Before',4),
-(16,'Expresso Love',5),
-(17,'Mtv',5),
-(18,'Romeo and Juliet',5);
-
-INSERT INTO `historico_reproducao` (`musica_id`, `usuario_id`)
-VALUES
-(1,1),
-(13,1),
-(9,2),
-(7,2),
+(1,4),
+(1,3),
+(2,1),
 (2,3),
+(3,2),
+(3,1),
+(4,4);
+
+INSERT INTO `musica` (`musica_id`, `nome`, `album_id`) VALUES
+(1,'Soul For Us',1),
+(2,'Reflections Of Magic',1),
+(3,'Dance With Her Own',1),
+(4,'Troubles Of My Inner Fire',2),
+(5,'Time Fireworks',2),
+(6,'Magic Circus',3),
+(7,'Honey, So Do I',3),
+(8,"Sweetie, Let's Go Wild",3),
+(9,'She Knows',3),
+(10,'Fantasy For Me',4),
+(11,'Celebration Of More',4),
+(12,'Rock His Everything',4),
+(13,'Home Forever',4),
+(14,'Diamond Power',4),
+(15,"Honey, Let's Be Silly",4),
+(16,'Thang Of Thunder',5),
+(17,'Words Of Her Life',5),
+(18,'Without My Streets',5);
+
+INSERT INTO `historico_reproducao` (`musica_id`, `usuario_id`) VALUES
+(1,1),
+(6,1),
+(14,1),
+(16,1),
+(13,2),
+(17,2),
+(2,2),
+(15,2),
+(4,3),
+(16,3),
 (6,3),
 (3,4),
-(8,4),
-(11,4),
-(15,2),
-(18,1),
-(4,3),
-(10,3),
-(5,1);
+(18,4),
+(11,4);
