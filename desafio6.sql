cedd400aaa5f2a19d3eb857638d9a73c569b8602
PAULO LINS <paulolins100@gmail.com>
2021-01-20 02:21:21 -0300

    desafio6fixagain

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,5 @@ CONVERT(ROUND(MAX(p.plano_valor),2), character) AS `faturamento_maximo`,
 convert(ROUND((SELECT SUM(p.plano_valor) FROM SpotifyClone.plano p
 INNER JOIN SpotifyClone.usuario u ON u.plano_id = p.plano_id)/(SELECT COUNT(plano_id) FROM SpotifyClone.usuario),2), character)
 AS `faturamento_medio`, CONVERT(SUM(p.plano_valor), CHARACTER) AS `faturamento_total`
-FROM SpotifyClone.plano p INNER JOIN SpotifyClone.usuario U
+FROM SpotifyClone.plano p INNER JOIN SpotifyClone.usuario u
 ON u.plano_id = p.plano_id;
