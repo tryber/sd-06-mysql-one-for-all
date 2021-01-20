7701ad2ea73690558269634930faee9e7601f14b
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 23:43:18 -0300

    refatoring

--- desafio9.sql
+++ desafio9.sql
@@ -2,10 +2,10 @@ DELIMETER $$
 CREATE PROCEDURE albuns_do_artista(IN artists VARCHAR(40))
 BEGIN
     SELECT artists.artist_name AS artista,
-    albuns.album_name AS album
+    albums.album_name AS album
     FROM artists
-    JOIN albuns
-    ON albuns.artist_id = artists.artist_id
+    JOIN albums
+    ON albums.artist_id = artists.artist_id
     WHERE artists.artist_name = artists;
 END; $$
 DELIMETER ;
