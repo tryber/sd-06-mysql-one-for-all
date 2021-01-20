694139a68cd0e56cf772a42f77b5737813aa6c39
ilanribeiro <ilanribeiro@gmail.com>
2021-01-20 05:08:03 -0300

    feature: requirement 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+usuario.nome_usuario AS 'usuario',
+musica.nome_musica AS 'nome'
+FROM SpotifyClone.historico_de_reproducao AS historico
+INNER JOIN SpotifyClone.usuario AS usuario
+ON historico.usuario_id = usuario.usuario_id
+INNER JOIN SpotifyClone.musica AS musica
+ON historico.musica_id = musica.musica_id
+ORDER BY
+usuario.nome,
+musica.nome;
