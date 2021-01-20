9354948e5ac7b58fee8fd2f404c4a9d0fb7a88a9
ilanribeiro <ilanribeiro@gmail.com>
2021-01-19 22:23:30 -0300

    change: modifying default identation workbench to verify if pass in test

--- desafio1.sql
+++ desafio1.sql
@@ -6,64 +6,64 @@ USE SpotifyClone;
 
 DROP TABLE IF EXISTS `plano`;
 CREATE TABLE `plano` (
-  `plano_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `valor_plano` DECIMAL(4,2) NOT NULL,
+`plano_id` INT PRIMARY KEY AUTO_INCREMENT,
+`nome` VARCHAR(100) NOT NULL,
+`valor_plano` DECIMAL(4,2) NOT NULL,
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
-  `usuario_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `idade` INT NOT NULL,
-  `plano_id` INT NOT NULL,
-  FOREIGN KEY (`plano_id`) REFERENCES `plano` (`plano_id`)
+`usuario_id` INT PRIMARY KEY AUTO_INCREMENT,
+`nome` VARCHAR(100) NOT NULL,
+`idade` INT NOT NULL,
+`plano_id` INT NOT NULL,
+FOREIGN KEY (`plano_id`) REFERENCES `plano` (`plano_id`)
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `artista`;
 CREATE TABLE `artista` (
-  `artista_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL
+`artista_id` INT PRIMARY KEY AUTO_INCREMENT,
+`nome` VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `album`;
 CREATE TABLE `album` (
-  `album_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `artista_id` INT NOT NULL,
-  FOREIGN KEY (`artista_id`) REFERENCES `artista` (`artista_id`)
+`album_id` INT PRIMARY KEY AUTO_INCREMENT,
+`nome` VARCHAR(100) NOT NULL,
+`artista_id` INT NOT NULL,
+FOREIGN KEY (`artista_id`) REFERENCES `artista` (`artista_id`)
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `segue_artistas`;
 CREATE TABLE `segue_artistas` (
-  `usuario_id` INT NOT NULL,
-  `artista_id` INT NOT NULL,
-  PRIMARY KEY (`usuario_id`, `artista_id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
-  FOREIGN KEY (`artista_id`) REFERENCES `artista` (`artista_id`)
+`usuario_id` INT NOT NULL,
+`artista_id` INT NOT NULL,
+PRIMARY KEY (`usuario_id`, `artista_id`),
+FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
+FOREIGN KEY (`artista_id`) REFERENCES `artista` (`artista_id`)
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `musica`;
 CREATE TABLE `musica` (
-  `musica_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `album_id` INT NOT NULL,
-  FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`)
+`musica_id` INT PRIMARY KEY AUTO_INCREMENT,
+`nome` VARCHAR(100) NOT NULL,
+`album_id` INT NOT NULL,
+FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`)
 ) ENGINE = InnoDB;
 
 
 DROP TABLE IF EXISTS `historico_reproducao`;
 CREATE TABLE `historico_reproducao` (
-  `musica_id` INT NOT NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`musica_id`, `usuario_id`),
-  FOREIGN KEY (`musica_id`) REFERENCES `musica` (`musica_id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
+`musica_id` INT NOT NULL,
+`usuario_id` INT NOT NULL,
+PRIMARY KEY (`musica_id`, `usuario_id`),
+FOREIGN KEY (`musica_id`) REFERENCES `musica` (`musica_id`),
+FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
 ) ENGINE = InnoDB;
 
 -- Inserindo dados nas tabelas
@@ -125,7 +125,6 @@ INSERT INTO `musica` (`musica_id`, `nome`, `album_id`) VALUES
 (15, 'The Night Before', 4),
 (16, 'Expresso Love', 7),
 (17, 'Romeo and Juliet', 7);
-
 
 INSERT INTO `historico_reproducao` (`musica_id`, `usuario_id`) VALUES
 (1, 1),
