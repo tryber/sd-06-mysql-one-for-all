874f3155e3c1210d81081728df401ceca60a59d0
PAULO LINS <paulolins100@gmail.com>
2021-01-20 05:18:47 -0300

    desafio11-vendoseassimpassa...

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,10 @@
 CREATE VIEW cancoes_premium AS
-SELECT c.cancoes_name AS `nome`,
-COUNT(h.usuario_id) AS `reproducoes`
-FROM spotifyclone.cancoes AS c
-INNER JOIN SpotifyClone.historico AS h
-ON c.cancoes_id=h.cancoes_id
-INNER JOIN SpotifyClone.usuario AS u ON u.usuario_id=h.usuario_id
-WHERE u.plano_id = 2 or u.plano_id = 3
-GROUP BY c.cancoes_name
-ORDER BY `nome`;
+SELECT
+SpotifyClone.cancoes.cancoes_name AS 'nome',
+COUNT(SpotifyClone.historico.usuario_id) AS `reproducoes`
+FROM SpotifyClone.cancoes, SpotifyClone.historico, SpotifyClone.usuario
+WHERE SpotifyClone.cancoes.cancoes_id = SpotifyClone.historico.cancoes_id
+AND SpotifyClone.usuario.usuario_id = SpotifyClone.historico.usuario_id
+AND SpotifyClone.usuario.plano_id IN (2, 3)
+GROUP BY SpotifyClone.cancoes.cancoes_name
+ORDER BY SpotifyClone.cancoes.cancoes_name;
