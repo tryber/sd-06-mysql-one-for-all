2b94e3561920c467235caa273c43685491935db6
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 23:31:55 -0300

    Requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,10 @@
-DELIMITER $$
+DELIMETER $$
 CREATE PROCEDURE albuns_do_artista(IN artists varchar(50)) 
 BEGIN
-    select a.nome AS `artista`,
-    al.nome AS `album` FROM artists
+    select artists.nome AS `artista`,
+    albums.album_name AS `album` 
+    FROM artists
     JOIN albums ON albums.artist_id = artists.artist_id
     WHERE artists.artist_name = artists;
 END $$
-DELIMITER ;
+DELIMETER ;
