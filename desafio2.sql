5ed02b780fa9905ec1951a8d4f163dcec2f91e00
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 22:17:56 -0300

    Code Climate

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,9 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT 
-	COUNT(DISTINCT m.music_name) AS cancoes,
-	COUNT(DISTINCT a.artist_name) AS artistas,
-	COUNT(DISTINCT al.album_name) AS albums
+COUNT(DISTINCT m.music_name) AS cancoes,
+COUNT(DISTINCT a.artist_name) AS artistas,
+COUNT(DISTINCT al.album_name) AS albums
 FROM 
-	SpotifyClone.musics AS m,
-	SpotifyClone.artists AS a,
-	SpotifyClone.albums AS al;
-    
+SpotifyClone.musics AS m,
+SpotifyClone.artists AS a,
+SpotifyClone.albums AS al;
