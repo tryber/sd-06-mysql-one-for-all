66c3b5859f8fee7d07d2ca842354235ead14f0e0
Matheuscara <matheuscara2000@gmail.com>
2021-01-19 23:29:15 -0300

    Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,8 @@
-DELIMITER $$
+DELIMETER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users FOR EACH ROW
 BEGIN
     DELETE FROM histories WHERE histories.user_id = OLD.user_id;
     DELETE FROM follows WHERE follows.user = OLD.user_id;
 END; $$
-DELIMITER ;
+DELIMETER ;
