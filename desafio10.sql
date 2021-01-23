1bbd7e7dfb0a825a3bbd2b40cfd9dcea3f4c0f2a
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-01-23 19:52:44 -0300

    fix desafio10 sql

--- desafio10.sql
+++ desafio10.sql
@@ -1,11 +1,13 @@
+USE SpotifyClone;
+
 DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(userId INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE total_played INT;
     SELECT COUNT(p.user_id)
-    FROM played AS p
+    FROM SpotifyClone.played AS p
     WHERE p.user_id = userId INTO total_played;
-    RETURN total_reproduction;
+    RETURN total_played;
 END; $$
 DELIMITER ;
