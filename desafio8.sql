fef7aab280bdd619dc716f78e61d50a6f35336a9
William Dieter <williamzdieter@gmail.com>
2021-01-28 21:17:04 -0300

    Testing 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,16 @@
+USE SpotifyClone;
+
 DELIMITER $$
-DROP TRIGGER IF EXISTS trigger_usuario_delete;
-CREATE TRIGGER trigger_usuario_delete BEFORE DELETE
-ON User
-FOR EACH ROW
+
+CREATE TRIGGER trigger_usuario_delete
+  AFTER DELETE ON users
+  FOR EACH ROW
 BEGIN
-DELETE FROM Reproduction_History WHERE user_id = OLD.user_id;
-DELETE FROM Following_Musicians WHERE user_id = OLD.user_id;
-END;
-$$ DELIMITER ;
+  SET @user_id = OLD.id;
+
+  DELETE FROM follows WHERE user_id = @user_id;
+
+  DELETE FROM song_plays WHERE user_id = @user_id;
+END; $$
+
+DELIMITER ;
