dae4b2ca98da90c0c9519eda6a2742ea0689b24e
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-05 09:38:50 -0300

    :sparkles: DESAFIO 6
    Exibindo o faturamento easy

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
+ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
+ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
+ROUND(SUM(p.plan_value), 2) AS faturamento_total
+FROM SpotifyClone.plans AS p
+INNER JOIN SpotifyClone.users AS u
+ON p.plan_id = u.plan_id;
