6263564478118395a64f6dab2badffad339344dd
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-05 09:09:11 -0300

    :art: DESAFIO 3
    comparei historico com song id que estava oposto

--- desafio3.sql
+++ desafio3.sql
@@ -4,5 +4,5 @@ FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.history_songs AS hist
 ON u.user_id = hist.user_id
 INNER JOIN SpotifyClone.songs AS s
-ON s.song_id = hist.song_id
-ORDER BY u.user_name, s.song_name;
+ON hist.song_id = s.song_id
+ORDER BY `usuario`, `nome`;
