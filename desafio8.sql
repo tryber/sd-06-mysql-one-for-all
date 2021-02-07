c02e68ab531c12ce676e0e3b1d7808ee3b85cd6d
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-07 10:52:38 -0300

    :art: DESAFIO 8
    deleted para OLD linha 7 e 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON users
 FOR EACH ROW
 BEGIN
-DELETE FROM followers WHERE user_id = DELETED.user_id
-DELETE FROM history_songs WHERE user_id = DELETED.user_id
+DELETE FROM followers WHERE user_id = OLD.user_id
+DELETE FROM history_songs WHERE user_id = OLD.user_id
 END;
 // DELIMITER ;
