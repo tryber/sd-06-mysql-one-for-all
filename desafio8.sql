d2455706ded75aefb19f863bd3f6a977d45c254e
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-07 11:04:07 -0300

    :art: DESAFIO 8
    Troquei AFTER por BEFORE

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 DROP TRIGGER IF EXISTS trigger_usuario_delete;
 DELIMITER //
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON users
+BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
 DELETE FROM followers WHERE user_id = OLD.user_id;
