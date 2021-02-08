18d4ef256fdb1ee0bcfb163e6d39bb25129ef15c
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-02-08 18:52:41 -0300

    1 ao 7 done

---
+++ challenges/desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+a.artist_name AS `artista`,
+COUNT(f.artist_id) AS `seguidores`
+FROM SpotifyClone.Artists AS a
+INNER JOIN SpotifyClone.Followers AS f
+ON a.artist_id = f.artist_id
+GROUP BY f.artist_id
+ORDER BY `seguidores` DESC, `artista`
+LIMIT 3;
