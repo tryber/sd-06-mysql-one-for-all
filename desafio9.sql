27cb1cb95e22ea7257bc6d31cac9133d6f18f78d
Dan <dansantosalfa@gmail.com>
2021-02-07 21:47:34 -0300

    desafio 6-9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE SpotifyClone.albuns_do_artista (IN nome VARCHAR(100))
+BEGIN 
+SELECT 
+  ar.artista, al.album
+FROM artistas ar
+  INNER JOIN albuns al ON ar.id_artista = al.id_artista
+  WHERE ar.artista = nome
+  ORDER BY ar.artista; 
+END $$
+DELIMITER;
