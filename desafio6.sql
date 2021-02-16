7a15f4e7212606fb5388861272375ed54f12a586
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-16 16:17:53 -0300

    Requirements 06 - push 02 - Round position fixed

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-  MIN(ROUND(p.plan_value, 2)) AS "faturamento_minimo",
-  MAX(ROUND(p.plan_value, 2)) AS "faturamento_maximo",
-  AVG(ROUND(p.plan_value, 2)) AS "faturamento_medio",
-  SUM(ROUND(p.plan_value, 2)) AS "faturamento_total"
+  ROUND(MIN(p.plan_value), 2) AS "faturamento_minimo",
+  ROUND(MAX(p.plan_value), 2) AS "faturamento_maximo",
+  ROUND(AVG(p.plan_value), 2) AS "faturamento_medio",
+  ROUND(SUM(p.plan_value), 2) AS "faturamento_total"
 FROM SpotifyClone.plans AS p
   INNER JOIN SpotifyClone.users AS u
   ON p.plan_id = u.plan_id;
