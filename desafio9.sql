6b3bc87e9cfcb511fbbf5f14a271dabdf9aca6a6
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-17 09:05:48 -0300

    Requirements 09 - push 02 - code fixes

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
 
-CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(64))
+CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(60))
 BEGIN
   SELECT
     ar.artist_name AS "artista",
     al.album_name AS "album"
   FROM SpotifyClone.artists AS ar
-    LEFT JOIN SpotifyClone.Albums AS al
+    INNER JOIN SpotifyClone.albuns AS al
       ON al.artist_id = ar.artist_id
   WHERE ar.artist_name = artist
   ORDER BY `album`;
