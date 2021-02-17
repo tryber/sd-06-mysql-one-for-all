342f0a5cab89731b13849cabc6a4bb06e6858f51
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-17 08:56:21 -0300

    Requirements 10 - push 01

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico (usuario int)
+CREATE FUNCTION quantidade_musicas_no_historico (user int)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quantidade_musicas_no_historico int;
+  DECLARE quantidade_musicas_no_historico int;
 SELECT COUNT(*) 
-FROM SpotifyClone.historico_de_reproducoes
-WHERE historico_de_reproducoes.usuario_id = usuario
-GROUP BY historico_de_reproducoes.usuario_id
+  FROM SpotifyClone.songs_history AS sh
+  WHERE sh.user_id = user
+GROUP BY sh.user_id
 INTO quantidade_musicas_no_historico;
 RETURN quantidade_musicas_no_historico;
 END $$
