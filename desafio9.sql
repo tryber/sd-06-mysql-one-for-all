233702b360eac85e4d675976bc290e44bd021fa0
Isabela Rosa <isabelarosa954@gmail.com>
2021-02-21 21:46:55 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE albuns_do_artista(nome_artista VARCHAR(100))
+SELECT
+  A.nome_artista AS `artista`,
+  AB.nome_album AS `album`
+FROM SpotifyClone.artista AS A
+INNER JOIN SpotifyClone.album AS AB ON A.artista_id = AB.artista_id
+WHERE A.nome_artista = nome_artista;
